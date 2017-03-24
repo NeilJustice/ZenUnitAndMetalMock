fe27770dde42ddc6ce6b5c60eb23411a0e681b5a
Neil Justice <njjustice@gmail.com>
2017-03-24 17:14:13 -0500

    Upgrade CMakeMacros.cmake and ZenUnitTestedProgram

--- ZenUnitTestedProgram/StaticLibrary/Program.cpp
+++ ZenUnitTestedProgram/StaticLibrary/Program.cpp
@@ -1,23 +1,22 @@
+#include "pch.h"
 #include "Program.h"
-#include <algorithm>
-#include <iostream>
 
 int Program::Main(int argc, char* argv[]) const
 {
-   std::vector<std::string> args = ArgcArgvToStringVector(argc, argv);
+   vector<string> args = ArgcArgvToStringVector(argc, argv);
    return ArgsMain(args);
 }
 
-int Program::ArgsMain(const std::vector<std::string>&) const
+int Program::ArgsMain(const vector<string>&) const
 {
-   std::cout << "Hello ZenUnit-tested program\n";
+   cout << "Hello ZenUnit-tested program\n";
    return 0;
 }
 
-std::vector<std::string> Program::ArgcArgvToStringVector(int argc, char* argv[])
+vector<string> Program::ArgcArgvToStringVector(int argc, char* argv[])
 {
-	std::vector<std::string> args;
+	vector<string> args;
    args.reserve(argc);
-   std::for_each(argv, argv + argc, [&](char* arg) { args.emplace_back(arg); });
+   for_each(argv, argv + argc, [&](char* arg) { args.emplace_back(arg); });
 	return args;
 }
---
+++ ZenUnitTestedProgram/StaticLibrary/pch.h
@@ -0,0 +1,6 @@
+#pragma once
+#include <algorithm>
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
