3002987b8319957e963f3b3c36f229eec2e30594
Neil Justice <njjustice@gmail.com>
2017-03-27 03:54:21 -0500

    Add MinSizeRel to Linux and Windows build and install scripts

--- ZenUnitTestedProgram/StaticLibrary.Test/pch.h
+++ ZenUnitTestedProgram/StaticLibrary.Test/pch.h
@@ -1,3 +1,5 @@
-#pragma once
+#ifndef PCH_STATICLIBRARY_TEST
+#define PCH_STATICLIBRARY_TEST
 #include "ZenUnit/ZenUnit.h"
 #include "ZenUnit/ZenMock.h"
+#endif
--- ZenUnitTestedProgram/StaticLibrary/pch.h
+++ ZenUnitTestedProgram/StaticLibrary/pch.h
@@ -1,6 +1,8 @@
-#pragma once
+#ifndef PCH_STATICLIBRARY
+#define PCH_STATICLIBRARY
 #include <algorithm>
 #include <iostream>
 #include <string>
 #include <vector>
 using namespace std;
+#endif
