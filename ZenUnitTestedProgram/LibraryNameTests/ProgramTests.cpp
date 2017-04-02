e526061ce11142558fc814d776fd7ec8633c608a
Neil Justice <njjustice@gmail.com>
2017-04-02 16:08:06 -0500

    Improve example project ZenUnitTestedProgram

--- ZenUnitTestedProgram/LibraryNameTests/ProgramTests.cpp
+++ ZenUnitTestedProgram/LibraryNameTests/ProgramTests.cpp
@@ -4,6 +4,7 @@
 
 TESTS(ProgramTests)
 SPEC(Test)
+SPECX(ValueParameterizedTest)
 SPECEND
 
 TEST(Test)
@@ -14,6 +15,16 @@ TEST(Test)
    componentMock.VirtualFunction();
    //
    ZEN(componentMock.VirtualFunctionMock.AssertCalledOnce());
+}
+
+TEST3X3(ValueParameterizedTest,
+   int x, int y, int z,
+   0, 0, 0,
+   1, 1, 0,
+   1, 0, 1,
+   2, 1, 1)
+{
+   ARE_EQUAL(x, y + z);
 }
 
 }; RUN(ProgramTests)
