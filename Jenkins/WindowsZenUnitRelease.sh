65795773db68174eae6d061b861081411565c36d
Neil Justice <njjustice@gmail.com>
2017-04-02 16:02:03 -0500

    Jenkins script fixes

--- Jenkins/WindowsZenUnitRelease.sh
+++ Jenkins/WindowsZenUnitRelease.sh
@@ -2,4 +2,4 @@
 set -eu
 python ZenUnitPy/ZenUnitPy/BuildAndInstall.py "Visual Studio 14 2015 Win64" Release ZenUnit ZenUnit.Test "" -DCMAKE_INSTALL_PREFIX=C:/install Install
 cd ZenUnitTestedProgram
-python ../ZenUnitPy/ZenUnitPy/BuildAndInstall.py "Visual Studio 14 2015 Win64" Release ZenUnitTestedProgram StaticLibrary.Test "" -DCMAKE_INSTALL_PREFIX=. Install
+python ../ZenUnitPy/ZenUnitPy/BuildAndInstall.py "Visual Studio 14 2015 Win64" Release ZenUnitTestedProgram LibraryNameTests "" -DCMAKE_INSTALL_PREFIX=. Install
