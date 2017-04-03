38869f5418c740ba1f3bd815ab89e2ad625b8ef7
Neil Justice <njjustice@gmail.com>
2017-04-02 19:00:25 -0500

    Add clang-tidy to Jenkins/LinuxZenUnitDebugClang.sh

--- Jenkins/LinuxZenUnitDebugClang.sh
+++ Jenkins/LinuxZenUnitDebugClang.sh
@@ -2,7 +2,9 @@
 set -eu
 python3 ZenUnitPy/ZenUnitPy/CoveragePylintFlake8.py ZenUnitPy/ZenUnitPy.Test RunAllWithCoverage.py
 export CXX=/usr/bin/clang++
-python3 ZenUnitPy/ZenUnitPy/BuildAndInstall.py Ninja Debug ZenUnit ZenUnit.Test "clang version" -DCMAKE_INSTALL_PREFIX=/usr/local Install
+python3 ZenUnitPy/ZenUnitPy/BuildAndInstall.py Ninja Debug ZenUnit ZenUnit.Test "clang version" "-DClangTidyMode=ON -DCMAKE_INSTALL_PREFIX=/usr/local" Install
 cd ZenUnitTestedProgram
 python3 ../ZenUnitPy/ZenUnitPy/BuildAndInstall.py Ninja Debug ZenUnitTestedProgram LibraryNameTests "" -DCMAKE_INSTALL_PREFIX=. Install
 cd ..
+cmake --build . --target clang-tidy
+
