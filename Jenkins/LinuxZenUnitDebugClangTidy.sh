f68db571fd4736e3c2385fcbfb70c2654bc117a0
Neil Justice <njjustice@gmail.com>
2017-04-07 18:29:53 -0500

    Upgrade ZenUnitHelloWorld project

--- Jenkins/LinuxZenUnitDebugClangTidy.sh
+++ Jenkins/LinuxZenUnitDebugClangTidy.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 set -eu
 CXX=/usr/local/bin/clang++ cmake -H. -BDebug -GNinja -DCMAKE_BUILD_TYPE=Debug -DClangTidyMode=ON
-cd Debug
-time ninja clang-tidy
-
+(cd Debug; time ninja clang-tidy)
