cf8a694e58f5e9378e8caa151dbcfa94c18dcbc4
Neil Justice <njjustice@gmail.com>
2017-04-08 19:35:06 -0500

    Upgrade ZenUnitHelloWorld project

--- Jenkins/LinuxZenUnitDebugClang.sh
+++ Jenkins/LinuxZenUnitDebugClang.sh
@@ -4,4 +4,4 @@ python3 ZenUnitPy/ZenUnitPy/CoveragePylintFlake8.py ZenUnitPy/ZenUnitPy.Test Run
 export CXX=/usr/bin/clang++
 python3 ZenUnitPy/ZenUnitPy/BuildAndInstall.py Ninja Debug ZenUnit ZenUnit.Test "clang version" "-DClangTidyMode=ON -DCMAKE_INSTALL_PREFIX=/usr/local" Install
 cd ZenUnitHelloWorld
-python3 ../ZenUnitPy/ZenUnitPy/BuildAndInstall.py Ninja Debug ZenUnitHelloWorld LibraryNameTests "clang version" "" NoInstall
+python3 ../ZenUnitPy/ZenUnitPy/BuildAndInstall.py Ninja Debug LibraryName LibraryNameTests "" "" NoInstall
