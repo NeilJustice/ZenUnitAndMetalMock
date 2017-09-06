2415914cdd2f14b93b883c8164db7647402a6b02
Neil Justice <njjustice@gmail.com>
2017-09-06 12:57:54 -0500

    Custom-built clang++ to Fedora 26 clang++

--- Jenkins/LinuxZenUnitDebugClangSanitizers.sh
+++ Jenkins/LinuxZenUnitDebugClangSanitizers.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -eu
-export CXX=/usr/local/bin/clang++
 UBSAN_OPTIONS=halt_on_error=true python3 ZenUnitPy/ZenUnitPy/BuildAndInstall.py Ninja Debug ZenUnit ZenUnitTests "clang version" -DSanitizersMode=ON NoInstall
+
