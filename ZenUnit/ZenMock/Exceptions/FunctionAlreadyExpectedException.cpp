2d6ac05a5dc7d22690c91fbdc98315ff9854b976
Neil Justice <njjustice@gmail.com>
2017-09-07 08:11:02 -0500

    Add raw string delimiters

--- ZenUnit/ZenMock/Exceptions/FunctionAlreadyExpectedException.cpp
+++ ZenUnit/ZenMock/Exceptions/FunctionAlreadyExpectedException.cpp
@@ -12,7 +12,7 @@ namespace ZenMock
    string FunctionAlreadyExpectedException::MakeWhat(const std::string& zenMockedFunctionSignature)
    {
       const string what = "For ZenMocked function \"" + zenMockedFunctionSignature + R"%(":
-Already called [ZenMockedFunctionName]Mock.Expect[AndReturn|AndReturnValues|AndThrow]().%)";
+Already called [ZenMockedFunctionName]Mock.Expect[AndReturn|AndReturnValues|AndThrow]().)%";
       return what;
    }
 
