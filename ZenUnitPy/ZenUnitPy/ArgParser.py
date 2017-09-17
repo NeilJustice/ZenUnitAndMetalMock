d4cf0ec38224fac1bec697377a927922492528d2
Neil Justice <njjustice@gmail.com>
2017-09-17 00:54:52 -0500

    Redesign Jenkins Python scripts

--- ZenUnitPy/ZenUnitPy/ArgParser.py
+++ ZenUnitPy/ZenUnitPy/ArgParser.py
@@ -1,2 +1,6 @@
-def parse_arg(argValue, argName):
-   pass
+def parse_arg(argName, argValue):
+   if not argValue.startswith(f'{argName}='):
+      raise ValueError(f"Argument '{argValue}' does not start with '{argName}='")
+   indexOfFirstEqualsSign = argValue.index('=')
+   parsedArgValue = argValue[indexOfFirstEqualsSign + 1:]
+   return parsedArgValue
