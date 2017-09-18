fb45054313d23ebb6cdf8b7862c2921bcbbeae00
Neil Justice <njjustice@gmail.com>
2017-09-17 22:45:27 -0500

    Redesign Jenkins Python scripts

--- Jenkins/LinuxZenUnitDebugClang.sh
+++ Jenkins/LinuxZenUnitDebugClang.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
 python3 ZenUnitPy/ZenUnitPy/RunTestsWithCoverageAndPylintFlake8.py
-CXX=/usr/bin/clang++ python3 ZenUnitPy/ZenUnitPy/BuildZenUnit.py --generator=Ninja --buildType=Debug --definitions=\"\" --installDirectory=/usr/local
-CXX=/usr/bin/clang++ python3 ZenUnitPy/ZenUnitPy/BuildZenUnitHelloWorld.py --generator=Ninja --buildType=Debug --definitions=\"\"
+CXX=/usr/bin/clang++ python3 ZenUnitPy/ZenUnitPy/BuildZenUnit.py --generator=Ninja --buildType=Debug --definitions="" --installDirectory=/usr/local
+CXX=/usr/bin/clang++ python3 ZenUnitPy/ZenUnitPy/BuildZenUnitHelloWorld.py --generator=Ninja --buildType=Debug --definitions=""
--- Jenkins/LinuxZenUnitReleaseGCC.sh
+++ Jenkins/LinuxZenUnitReleaseGCC.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-CXX=/usr/bin/g++ python3 ZenUnitPy/ZenUnitPy/BuildZenUnit.py --generator=Ninja --buildType=Debug --definitions="" --installDirectory=/usr/local
+CXX=/usr/bin/g++ python3 ZenUnitPy/ZenUnitPy/BuildZenUnit.py --generator=Ninja --buildType=Release --definitions="" --installDirectory=/usr/local
