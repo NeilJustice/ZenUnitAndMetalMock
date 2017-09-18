85a0c1c9b44f1c5d342978e436a79a6fe6f870c7
Neil Justice <njjustice@gmail.com>
2017-09-17 22:38:35 -0500

    Redesign Jenkins Python scripts

--- Jenkins/LinuxZenUnitDebugGCC.sh
+++ Jenkins/LinuxZenUnitDebugGCC.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
-CXX=/usr/bin/g++ python3 ZenUnitPy/ZenUnitPy/BuildZenUnit.py --generator=Ninja --buildType=Debug --definitions="" --installDirectory=NoInstall
+CXX=/usr/bin/g++ python3 ZenUnitPy/ZenUnitPy/BuildZenUnit.py --generator=Ninja --buildType=Debug --definitions="" --installDirectory=/usr/local
 CXX=/usr/bin/g++ python3 ZenUnitPy/ZenUnitPy/BuildZenUnitHelloWorld.py --generator=Ninja --buildType=Debug --definitions=""
--- Jenkins/LinuxZenUnitReleaseGCC.sh
+++ Jenkins/LinuxZenUnitReleaseGCC.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-CXX=/usr/bin/g++ python3 ZenUnitPy/ZenUnitPy/BuildZenUnit.py --generator=Ninja --buildType=Debug --definitions="" --installDirectory=NoInstall
+CXX=/usr/bin/g++ python3 ZenUnitPy/ZenUnitPy/BuildZenUnit.py --generator=Ninja --buildType=Debug --definitions="" --installDirectory=/usr/local
