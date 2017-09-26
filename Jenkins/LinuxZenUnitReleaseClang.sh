8bbb4ef5bf9c8336a75f5fcfce552a0d744cb8bc
Neil Justice <njjustice@gmail.com>
2017-09-25 22:51:46 -0500

    Disable Clang Linux Jenkins job install until std::optional compiles on non-customized Fedora 26

--- Jenkins/LinuxZenUnitReleaseClang.sh
+++ Jenkins/LinuxZenUnitReleaseClang.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
-CXX=/usr/bin/clang++ python3 ZenUnitPy/ZenUnitPy/BuildZenUnit.py --generator=Ninja --buildType=Release --definitions="" --installDirectory=/usr/local
+CXX=/usr/bin/clang++ python3 ZenUnitPy/ZenUnitPy/BuildZenUnit.py --generator=Ninja --buildType=Release --definitions="" --installDirectory=NoInstall
 CXX=/usr/bin/clang++ python3 ZenUnitPy/ZenUnitPy/BuildZenUnitHelloWorld.py --generator=Ninja --buildType=Release --definitions=""
