2db301f4327a97fd4838759114e27d19224a2d9b
Neil Justice <njjustice@gmail.com>
2017-10-04 22:10:31 -0500

    Add install step to ZenUnit 32-bit Jenkins jobs

--- Jenkins/WindowsZenUnitRelease32Bit.ps1
+++ Jenkins/WindowsZenUnitRelease32Bit.ps1
@@ -1,3 +1,3 @@
 $ErrorActionPreference = "Stop"
-python ZenUnitPy\ZenUnitPy\BuildZenUnit.py --generator="Visual Studio 15 2017" --buildType=Release --definitions="" --installDirectory=NoInstall
+python ZenUnitPy\ZenUnitPy\BuildZenUnit.py --generator="Visual Studio 15 2017" --buildType=Release --definitions="" --installDirectory=C:\install
 python ZenUnitPy\ZenUnitPy\BuildZenUnitHelloWorld.py --generator="Visual Studio 15 2017" --buildType=Release --definitions=""
