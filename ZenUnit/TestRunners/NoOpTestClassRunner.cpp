fc811a1b4188b1805a8ecf5b40a3f43ff39e13c9
Neil Justice <njjustice@gmail.com>
2017-11-06 11:50:08 -0600

    Single header groundwork

--- ZenUnit/TestRunners/NoOpTestClassRunner.cpp
+++ ZenUnit/TestRunners/NoOpTestClassRunner.cpp
@@ -18,7 +18,7 @@ namespace ZenUnit
       return TestClassResult();
    }
 
-   inline bool NoOpTestClassRunner::HasTestNameThatCaseInsensitiveMatchesPattern(const std::string& testNamePattern) const
+   inline bool NoOpTestClassRunner::HasTestNameThatCaseInsensitiveMatchesPattern(const std::string&) const
    {
       return false;
    }
