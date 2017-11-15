49ff707039cacdcdab8da680a62ae798fdd560f2
Neil Justice <njjustice@gmail.com>
2017-11-14 23:44:37 -0600

    Fix Linux includes

--- ZenUnit/ToStringer/ToStringer.cpp
+++ ZenUnit/ToStringer/ToStringer.cpp
@@ -1,8 +1,8 @@
-//#ifdef __linux__
-//#include "ZenUnit/pch.h"
-//#elif defined(_WIN64)
+#ifdef __linux__
+#include "ZenUnit/pch.h"
+#elif defined(_WIN64)
 #include "pch.h"
-//#endif
+#endif
 #include "ZenUnit/ToStringer/ToStringer.h"
 
 namespace ZenUnit
