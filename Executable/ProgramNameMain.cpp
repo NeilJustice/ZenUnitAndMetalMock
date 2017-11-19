31146d1c842ac207d8899ca94a213f0b90ebc643
Neil Justice <njjustice@gmail.com>
2017-11-19 12:07:57 -0600

    Changed printf to cout in example program

--- Executable/ProgramNameMain.cpp
+++ Executable/ProgramNameMain.cpp
@@ -1,3 +1,4 @@
+#include <iostream>
 #include <memory>
 #include <vector>
 using namespace std;
--- StaticLibrary/pch.h
+++ StaticLibrary/pch.h
@@ -1,5 +1,7 @@
 #pragma once
 #include <algorithm>
+#include <iostream>
 #include <memory>
+#include <string>
 #include <vector>
 using namespace std;
