0b3e5663c5db6cf6dc9e35acf1453853949acf76
Neil Justice <njjustice@gmail.com>
2018-04-16 21:20:39 -0500

    Moved commands in .travis.yml to CloudJobs/TravisCI.sh

---
+++ CloudJobs/TravisCI.sh
@@ -0,0 +1,11 @@
+set -eu
+
+curl https://raw.githubusercontent.com/NeilJustice/ZenMock/master/ZenMock/ZenMock.h -o ZenMock.h
+mkdir build
+cd build
+echo $CMAKE_BUILD_TYPE
+cmake -GNinja .. -Werror=dev -DCMAKE_CXX_COMPILER="$COMPILER" -DCMAKE_BUILD_TYPE="$CMAKE_BUILD_TYPE"
+ninja -v -j 4
+./ZenUnitTests/ZenUnitTests
+./ExampleTests/ExampleTests
+
