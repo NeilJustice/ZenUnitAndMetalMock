8e5232feb1c82e43954e20e33bb1ab6cc57785f5
Neil Justice <njjustice@gmail.com>
2018-04-17 07:54:18 -0500

    TravisCI test

--- CloudJobs/TravisCIZenUnit.sh
+++ CloudJobs/TravisCIZenUnit.sh
@@ -5,6 +5,7 @@ mkdir build
 cd build
 echo $CMAKE_BUILD_TYPE
 cmake -GNinja .. -Werror=dev -DCMAKE_CXX_COMPILER="$COMPILER" -DCMAKE_BUILD_TYPE="$CMAKE_BUILD_TYPE"
-ninja -v -j 2
+ninja -v -j 3
 ./ZenUnitTests/ZenUnitTests
 ./ExampleTests/ExampleTests
+
