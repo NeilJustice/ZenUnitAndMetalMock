cffa7c25ccb02aa71eaa3a5e787e6c0daea26bc6
Neil Justice <njjustice@gmail.com>
2018-05-18 23:09:35 -0500

    Screenshot update

--- ZenUnitTests/ZenUnitTestsMain.cpp
+++ ZenUnitTests/ZenUnitTestsMain.cpp
@@ -1,78 +1,78 @@
 #include "pch.h"
 
-const char* const Reason = "Test of skip feature";
+//const char* const Reason = "Test of skip feature";
 
-TESTS(SkippedTestsTestClass)
-AFACT(NonSkippedTest)
-FACTS(NonSkipped1X1Test)
-SKIPAFACT(SkippedNormalTest, Reason)
-SKIPFACTS(SkippedTest1X1, Reason)
-SKIPFACTS(SkippedTest2X2, Reason)
-EVIDENCE
-TEST(NonSkippedTest) {}
-TEST1X1(NonSkipped1X1Test, bool, true, false) {}
-TEST(SkippedNormalTest) {}
-TEST1X1(SkippedTest1X1, int, 0) {}
-TEST2X2(SkippedTest2X2, int, int, 0, 0) {}
-RUN_TESTS(SkippedTestsTestClass)
+// TESTS(SkippedTestsTestClass)
+// AFACT(NonSkippedTest)
+// FACTS(NonSkipped1X1Test)
+// SKIPAFACT(SkippedNormalTest, Reason)
+// SKIPFACTS(SkippedTest1X1, Reason)
+// SKIPFACTS(SkippedTest2X2, Reason)
+// EVIDENCE
+// TEST(NonSkippedTest) {}
+// TEST1X1(NonSkipped1X1Test, bool, true, false) {}
+// TEST(SkippedNormalTest) {}
+// TEST1X1(SkippedTest1X1, int, 0) {}
+// TEST2X2(SkippedTest2X2, int, int, 0, 0) {}
+// RUN_TESTS(SkippedTestsTestClass)
 
-TESTS(SkippedTestClassA)
-EVIDENCE
-SKIP_TESTS(SkippedTestClassA, Reason)
+// TESTS(SkippedTestClassA)
+// EVIDENCE
+// SKIP_TESTS(SkippedTestClassA, Reason)
 
-TESTS(SkippedTestClassB)
-EVIDENCE
-SKIP_TESTS(SkippedTestClassB, Reason)
+// TESTS(SkippedTestClassB)
+// EVIDENCE
+// SKIP_TESTS(SkippedTestClassB, Reason)
 
-template<typename T>
-TEMPLATE_TESTS(SkippedTemplateTestClassA, T)
-EVIDENCE
-SKIP_TEMPLATE_TESTS(SkippedTemplateTestClassA, Reason, int)
-THEN_SKIP_TEMPLATE_TESTS(SkippedTemplateTestClassA, Reason, double)
+// template<typename T>
+// TEMPLATE_TESTS(SkippedTemplateTestClassA, T)
+// EVIDENCE
+// SKIP_TEMPLATE_TESTS(SkippedTemplateTestClassA, Reason, int)
+// THEN_SKIP_TEMPLATE_TESTS(SkippedTemplateTestClassA, Reason, double)
 
-template<template<typename...> class MapType>
-TEMPLATE_TESTS(SkippedTemplateTestClassB, MapType)
-EVIDENCE
-RUN_TEMPLATE_TESTS(SkippedTemplateTestClassB, map)
-THEN_SKIP_TEMPLATE_TESTS(SkippedTemplateTestClassB, Reason, unordered_map)
+// template<template<typename...> class MapType>
+// TEMPLATE_TESTS(SkippedTemplateTestClassB, MapType)
+// EVIDENCE
+// RUN_TEMPLATE_TESTS(SkippedTemplateTestClassB, map)
+// THEN_SKIP_TEMPLATE_TESTS(SkippedTemplateTestClassB, Reason, unordered_map)
 
-class A
-{
-public:
-   virtual ~A() {}
-   virtual int f(int) { return 0; }
-};
+// class A
+// {
+// public:
+//    virtual ~A() {}
+//    virtual int f(int) { return 0; }
+// };
 
-struct AMock : public Zen::Mock<A>
-{
-   ZENMOCK_NONVOID1(int, f, int)
-};
+// struct AMock : public Zen::Mock<A>
+// {
+//    ZENMOCK_NONVOID1(int, f, int)
+// };
 
-TESTS(ZZTests)
-AFACT(VoidTest)
-FACTS(NXNTestA)
-FACTS(NXNTestB)
-EVIDENCE
+// TESTS(ZZTests)
+// AFACT(VoidTest)
+// FACTS(NXNTestA)
+// FACTS(NXNTestB)
+// EVIDENCE
 
-TEST(VoidTest)
-{
-}
+// TEST(VoidTest)
+// {
+// }
 
-TEST1X1(NXNTestA,
-   int,
-   0,
-   1)
-{
-}
+// TEST1X1(NXNTestA,
+//    int,
+//    0,
+//    1)
+// {
+// }
 
-TEST1X1(NXNTestB,
-   int,
-   0,
-   1)
-{
-}
+// TEST1X1(NXNTestB,
+//    int,
+//    0,
+//    1)
+// {
+// }
 
-RUN_TESTS(ZZTests)
+// RUN_TESTS(ZZTests)
 
 int main(int argc, char* argv[])
 {
