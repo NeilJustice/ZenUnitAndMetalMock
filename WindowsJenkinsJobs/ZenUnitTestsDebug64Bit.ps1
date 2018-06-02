07fd28fec7c7637ba6eb42ca505b2d0d2d0a8166
Neil Justice <njjustice@gmail.com>
2018-06-02 14:29:41 -0500

    Windows Jenkins job script upgrade

--- WindowsJenkinsJobs/ZenUnitTestsDebug64Bit.ps1
+++ WindowsJenkinsJobs/ZenUnitTestsDebug64Bit.ps1
@@ -1,3 +1,3 @@
-python.exe ZenUnitPy\ZenUnitPy\RunTestsWithCoverageAndLinting.py
+python.exe ZenUnitPy\ZenUnitPy\RunZenUnitPyTestsWithCoverageAndLinting.py
 if ($LastExitCode -ne 0) { exit $LastExitCode }
 python.exe ZenUnitPy\ZenUnitPy\BuildZenUnitOrZenMock.py --ZenUnitOrZenMock=ZenUnit --generator="Visual Studio 15 2017 Win64" --buildType=Debug --definitions=""
