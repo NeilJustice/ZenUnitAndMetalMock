0aefe14ae2221a40a92cbc36095b570ff6d73470
Neil Justice <njjustice@gmail.com>
2018-06-03 17:36:28 -0500

    Travis script update

--- CloudJobs/TravisCIZenUnit.sh
+++ CloudJobs/TravisCIZenUnit.sh
@@ -1,9 +1,9 @@
 set -eu
+# ZenUnitTests calls upon ZenMock to provide mocking capabilities to confirm the correctness of ZenUnit
+curl https://raw.githubusercontent.com/NeilJustice/ZenMock/master/ZenMock/ZenMock.h -o ZenMock.h
 mkdir build
 cd build
 cmake -GNinja .. -Werror=dev -DCMAKE_CXX_COMPILER="$COMPILER" -DCMAKE_BUILD_TYPE="$CMAKE_BUILD_TYPE"
-# ZenUnitTests call upon ZenMock to confirm the correctness of ZenUnit
-curl https://raw.githubusercontent.com/NeilJustice/ZenMock/master/ZenMock/ZenMock.h -o ZenMock.h
 ninja -v -j 3
 ./ZenUnitTests/ZenUnitTests
 ./ZenUnitExamples/ZenUnitExamples
