19371b217fb9c5a2f2aedf7c2fb6f7848b7c129f
Neil Justice <njjustice@gmail.com>
2018-06-15 08:13:05 -0500

    Travis CI GCC build fix

--- ZenUnitTests/Testing/TestUtil.h
+++ ZenUnitTests/Testing/TestUtil.h
@@ -7,21 +7,21 @@ namespace TestUtil
    {
       assert_true(sizeof...(StringTypes) > 0);
       std::ostringstream oss;
-      NewlineConcat(oss, strings...);
+      DoNewlineConcat(oss, strings...);
       std::string newlineConcatenatedValues = oss.str();
       newlineConcatenatedValues.erase(newlineConcatenatedValues.end() - 1);
       return newlineConcatenatedValues;
    }
 
    template<typename FirstStringType, typename... SubsequentStringTypes>
-   static void NewlineConcat(std::ostringstream& oss, FirstStringType firstString, SubsequentStringTypes... subsequentStrings)
+   static void DoNewlineConcat(std::ostringstream& oss, FirstStringType firstString, SubsequentStringTypes... subsequentStrings)
    {
       oss << firstString << '\n';
-      NewlineConcat(oss, subsequentStrings...);
+      DoNewlineConcat(oss, subsequentStrings...);
    }
 
    template<typename...>
-   static void NewlineConcat(std::ostringstream&)
+   static void DoNewlineConcat(std::ostringstream&)
    {
    }
 }
