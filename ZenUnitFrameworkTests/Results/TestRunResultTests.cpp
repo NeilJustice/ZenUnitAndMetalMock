60d55e56cbddde4b00d9e9aee6058aa651f20577
Neil Justice <njjustice@gmail.com>
2018-08-11 20:29:02 -0500

    Added Equalizer unit test

--- ZenUnitFrameworkTests/Results/TestRunResultTests.cpp
+++ ZenUnitFrameworkTests/Results/TestRunResultTests.cpp
@@ -416,10 +416,11 @@ namespace ZenUnit
 
    TEST(ZenUnitEqualizer_ThrowsIfAnyFieldNotEqual)
    {
-      //SETUP_EQUALIZER_THROWS_TEST(TestRunResult);
-      //EQUALIZER_THROWS_FOR_FIELD(TestRunResult, _testClassResults, vector<TestClassResult> { TestClassResult() });
-      //EQUALIZER_THROWS_FOR_FIELD(TestRunResult, _skippedTestClassNamesAndReasons, vector<string> { "" });
-      //EQUALIZER_THROWS_FOR_FIELD(TestRunResult, _skippedFullTestNamesAndReasons, vector<string> { "" });
+      SETUP_EQUALIZER_THROWS_TEST(TestRunResult);
+      EQUALIZER_THROWS_FOR_FIELD(TestRunResult, _testClassResults, vector<TestClassResult> { TestClassResult() });
+      EQUALIZER_THROWS_FOR_FIELD(TestRunResult, _skippedTestClassNamesAndReasons, vector<string> { "" });
+      EQUALIZER_THROWS_FOR_FIELD(TestRunResult, _skippedFullTestNamesAndReasons, vector<string> { "" });
+      EQUALIZER_THROWS_FOR_FIELD(TestRunResult, _numberOfFailedTestCases, 1);
    }
 
    RUN_TESTS(TestRunResultTests)
