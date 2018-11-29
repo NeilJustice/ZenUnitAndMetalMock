654d9e74a4b7071462869affaca7f9545622fcfa
Neil Justice <njjustice@gmail.com>
2018-11-28 22:48:57 -0600

    Added ZenUnit::RandomGenerator for testable random value generation

--- ZenUnit/ZenUnit.h
+++ ZenUnit/ZenUnit.h
@@ -1,7 +1,7 @@
 // C++ Unit Testing Library ZenUnit - Version 0.4.0
+// Written by Neil Justice
 //
 // The MIT License
-// Copyright 2018 Neil Justice
 // Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
 // The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
@@ -6412,6 +6412,27 @@ or change TEST(TestName) to TESTNXN(TestName, ...), where N can be 1 through 10.
       }
       return randomUnorderedSet;
    }
+
+   class RandomGenerator
+   {
+   public:
+      virtual bool RandomBool() const
+      {
+         return ZenUnit::Random<bool>();
+      }
+
+      virtual unsigned RandomUnsigned() const
+      {
+         return ZenUnit::Random<unsigned>();
+      }
+
+      virtual std::string RandomString() const
+      {
+         return ZenUnit::Random<std::string>();
+      }
+
+      virtual ~RandomGenerator() = default;
+   };
 
    inline int RunTests(int argc, char* argv[])
    {
