8805ba231a07eb64c96af7f1312597c30d11a476
Neil Justice <njjustice@gmail.com>
2019-02-04 21:50:52 -0600

    Added dot to CMake args

--- JenkinsJobs/Windows/InstallZenUnit.ps1
+++ JenkinsJobs/Windows/InstallZenUnit.ps1
@@ -1,3 +1,3 @@
-cmake.exe -G"Visual Studio 15 2017 Win64" -DCMAKE_INSTALL_PREFIX=C:\install
+cmake.exe -G"Visual Studio 15 2017 Win64" -DCMAKE_INSTALL_PREFIX=C:\install .
 if ($LastExitCode -ne 0) { exit $LastExitCode }
 cmake.exe --build . --target install
