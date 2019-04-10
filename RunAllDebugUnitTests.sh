fab1591515b09089b669910e0695ef42e0689756
Neil Justice <njjustice@gmail.com>
2019-04-09 21:40:57 -0500

    Fixed the Linux build

---
+++ RunAllDebugUnitTests.sh
@@ -0,0 +1,6 @@
+./Debug/ZenMockExamples/ZenMockExamples
+./Debug/ZenMockTests/ZenMockTests
+./Debug/ZenUnitExamples/ZenUnitExamples
+./Debug/ZenUnitLibraryTests/ZenUnitLibraryTests
+./Debug/ZenUnitUtilsAndAssertionTests/ZenUnitUtilsAndAssertionTests
+
---
+++ RunAllReleaseUnitTests.sh
@@ -0,0 +1,6 @@
+./Release/ZenMockExamples/ZenMockExamples
+./Release/ZenMockTests/ZenMockTests
+./Release/ZenUnitExamples/ZenUnitExamples
+./Release/ZenUnitLibraryTests/ZenUnitLibraryTests
+./Release/ZenUnitUtilsAndAssertionTests/ZenUnitUtilsAndAssertionTests
+
