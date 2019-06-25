21b493b86f688a43ec026a29a749572cd666d08b
Neil Justice <Neil.Justice@Emerson.com>
2019-06-25 13:04:20 -0500

    Windows Jenkins job script updates

--- JenkinsJobs/Windows/ZenUnitAndZenMockTestsDebug32Bit.ps1
+++ JenkinsJobs/Windows/ZenUnitAndZenMockTestsDebug32Bit.ps1
@@ -1,1 +1,1 @@
-python.exe ZenUnitPy\ZenUnitPy\BuildZenUnitAndZenMock.py --cmake-generator="Visual Studio 16 2019" --cmake-build-type=Debug --cmake-definitions=""
+python.exe ZenUnitPy\ZenUnitPy\BuildZenUnitAndZenMock.py --cmake-generator="Visual Studio 16 2019" --cmake-arch="Win32" --cmake-build-type=Debug --cmake-definitions=""
--- JenkinsJobs/Windows/ZenUnitAndZenMockTestsRelease64Bit.ps1
+++ JenkinsJobs/Windows/ZenUnitAndZenMockTestsRelease64Bit.ps1
@@ -1,1 +1,1 @@
-python.exe ZenUnitPy\ZenUnitPy\BuildZenUnitAndZenMock.py --cmake-generator="Visual Studio 16 2019 Win64" --cmake-build-type=Release --cmake-definitions=""
+python.exe ZenUnitPy\ZenUnitPy\BuildZenUnitAndZenMock.py --cmake-generator="Visual Studio 16 2019" --cmake-arch="x64" --cmake-build-type=Release --cmake-definitions=""
