762e6c7f51ec52f3c4e8a1cb6b6322c95596adf8
Neil Justice <Neil.Justice@Emerson.com>
2019-06-26 19:30:14 -0500

    Added Set-PSDebug -Trace 1 to Windows Jenkins scripts

--- JenkinsJobs/Windows/ZenUnitAndZenMockTestsDebug64Bit.ps1
+++ JenkinsJobs/Windows/ZenUnitAndZenMockTestsDebug64Bit.ps1
@@ -1,1 +1,3 @@
-python.exe ZenUnitPy\ZenUnitPy\BuildZenUnitAndZenMock.py --cmake-generator="Visual Studio 16 2019" --cmake-arch="x64" --cmake-build-type=Debug --cmake-definitions=""
+Set-PSDebug -Trace 1
+python.exe ZenUnitPy\ZenUnitPy\BuildZenUnitAndZenMock.py --cmake-generator="Visual Studio 16 2019" --cmake-arch="x64" --cmake-build-type="Debug" --cmake-definitions=""
+Set-PSDebug -Trace 0
--- JenkinsJobs/Windows/ZenUnitAndZenMockTestsRelease32Bit.ps1
+++ JenkinsJobs/Windows/ZenUnitAndZenMockTestsRelease32Bit.ps1
@@ -1,1 +1,3 @@
-python.exe ZenUnitPy\ZenUnitPy\BuildZenUnitAndZenMock.py --cmake-generator="Visual Studio 16 2019" --cmake-arch="Win32" --cmake-build-type=Release --cmake-definitions=""
+Set-PSDebug -Trace 1
+python.exe ZenUnitPy\ZenUnitPy\BuildZenUnitAndZenMock.py --cmake-generator="Visual Studio 16 2019" --cmake-arch="Win32" --cmake-build-type="Release" --cmake-definitions=""
+Set-PSDebug -Trace 0
