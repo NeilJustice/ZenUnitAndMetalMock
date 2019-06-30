b670934e6d3a3bc38e9d534e68affa07b4012732
Neil Justice <Neil.Justice@Emerson.com>
2019-06-30 08:48:50 -0500

    Add CMake install prefix to JenkinsJobs\Windows\ZenUnitAndZenMockTestsDebug64Bit.ps1

--- JenkinsJobs/Windows/InstallZenUnit.ps1
+++
@@ -1,9 +0,0 @@
-Set-PSDebug -Trace 1
-
-cmake.exe -G"Visual Studio 16 2019 Win64" -DCMAKE_INSTALL_PREFIX=C:\install .
-Set-PSDebug -Trace 0
-if ($LastExitCode -ne 0) { exit $LastExitCode }
-
-cmake.exe --build . --target install
-
-Set-PSDebug -Trace 0
--- JenkinsJobs/Windows/ZenUnitAndZenMockTestsDebug64Bit.ps1
+++ JenkinsJobs/Windows/ZenUnitAndZenMockTestsDebug64Bit.ps1
@@ -1,3 +1,5 @@
+param([string]$installFolder = "C:\install")
+
 Set-PSDebug -Trace 1
-python.exe ZenUnitPy\ZenUnitPy\BuildZenUnitAndZenMock.py --cmake-generator="Visual Studio 16 2019" --cmake-arch="x64" --cmake-build-type="Debug" --cmake-definitions=""
+python.exe ZenUnitPy\ZenUnitPy\BuildZenUnitAndZenMock.py --cmake-generator="Visual Studio 16 2019" --cmake-arch="x64" --cmake-build-type="Debug" --cmake-definitions="-DCMAKE_INSTALL_PREFIX=$installFolder"
 Set-PSDebug -Trace 0
