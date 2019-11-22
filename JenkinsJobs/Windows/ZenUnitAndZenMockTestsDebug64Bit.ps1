ace700b011110e86b175a6701d9cd5fffc116d63
Neil Justice <Neil.Justice@Emerson.com>
2019-11-21 21:13:35 -0600

    C:\install to C:\usr_local for Windows install folder

--- JenkinsJobs/Windows/ZenUnitAndZenMockTestsDebug64Bit.ps1
+++ JenkinsJobs/Windows/ZenUnitAndZenMockTestsDebug64Bit.ps1
@@ -1,4 +1,4 @@
-param([string]$installFolder = "C:\install")
+param([string]$installFolder = "C:\usr_local")
 
 Set-PSDebug -Trace 1
 python.exe ZenUnitPy\ZenUnitPy\BuildZenUnitAndZenMock.py --cmake-generator="Visual Studio 16 2019" --cmake-architecture="x64" --cmake-build-type="Debug" --cmake-definitions="-DCMAKE_INSTALL_PREFIX=$installFolder"
