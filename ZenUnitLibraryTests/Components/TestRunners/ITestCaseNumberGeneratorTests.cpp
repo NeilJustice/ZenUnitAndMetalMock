a8e724bb91920207381f760c45ce27c3bf0eda40
Neil Justice <Neil.Justice@Emerson.com>
2019-11-23 16:56:17 -0600

    Add unit tests for ITestCaseNumberGenerator::FactoryNew()

---
+++ ZenUnitLibraryTests/Components/TestRunners/ITestCaseNumberGeneratorTests.cpp
@@ -0,0 +1,20 @@
+#include "pch.h"
+
+TESTS(ITestCaseNumberGeneratorTests)
+AFACT(FactoryNew_RandomModeIsTrue_ReturnsNewRandomTestCaseNumberGeneratorAsSharedPointer)
+AFACT(FactoryNew_RandomModeIsFalse_ReturnsNewSequentialTestCaseNumberGeneratorAsSharedPointer)
+EVIDENCE
+
+TEST(FactoryNew_RandomModeIsTrue_ReturnsNewRandomTestCaseNumberGeneratorAsSharedPointer)
+{
+   std::shared_ptr<ITestCaseNumberGenerator> testCaseNumberGenerator = ITestCaseNumberGenerator::FactoryNew(true);
+   POINTEE_IS_EXACT_TYPE(RandomTestCaseNumberGenerator, testCaseNumberGenerator);
+}
+
+TEST(FactoryNew_RandomModeIsFalse_ReturnsNewSequentialTestCaseNumberGeneratorAsSharedPointer)
+{
+   std::shared_ptr<ITestCaseNumberGenerator> testCaseNumberGenerator = ITestCaseNumberGenerator::FactoryNew(false);
+   POINTEE_IS_EXACT_TYPE(SequentialTestCaseNumberGenerator, testCaseNumberGenerator);
+}
+
+RUN_TESTS(ITestCaseNumberGeneratorTests)
