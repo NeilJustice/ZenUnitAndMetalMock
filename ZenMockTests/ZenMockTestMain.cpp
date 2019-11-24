846ea16eb597bd79ba1ccd0afebc3a235c2dfa0c
Neil Justice <Neil.Justice@Emerson.com>
2019-11-23 21:56:39 -0600

    Remove testing code from ZenMockTestMain.cpp

--- ZenMockTests/ZenMockTestMain.cpp
+++ ZenMockTests/ZenMockTestMain.cpp
@@ -1,56 +1,5 @@
 #include "pch.h"
 
-class Mutex
-{
-public:
-   virtual void Lock() {}
-   virtual void Unlock() {}
-   virtual ~Mutex() = default;
-};
-
-class MutexMock : public Zen::Mock<Mutex>
-{
-public:
-   ZENMOCK_VOID0(Lock)
-   ZENMOCK_VOID0(Unlock)
-};
-
-class Action
-{
-public:
-   virtual void Act() {}
-   virtual ~Action() = default;
-};
-
-class ActionMock : public Zen::Mock<Action>
-{
-public:
-   ZENMOCK_VOID0(Act)
-};
-
-TESTS(Tests)
-AFACT(Test)
-EVIDENCE
-
-TEST(Test)
-{
-   MutexMock mutexMock;
-   ActionMock actionMock;
-   mutexMock.LockMock.Expect();
-   actionMock.ActMock.Expect();
-   mutexMock.UnlockMock.Expect();
-   //
-   mutexMock.Lock();
-   actionMock.Act();
-   mutexMock.Unlock();
-   //
-   ZENMOCK(mutexMock.LockMock.CalledOnce());
-   ZENMOCK(actionMock.ActMock.CalledOnce());
-   ZENMOCK(mutexMock.UnlockMock.CalledOnce());
-}
-
-RUN_TESTS(Tests)
-
 int main(int argc, char* argv[])
 {
    ZenUnitSelfTestMode::value = true;
