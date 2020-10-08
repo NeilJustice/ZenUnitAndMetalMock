1c18bf7573a33b39e843efb3d2618df22af4f630
Neil Justice <njjustice@gmail.com>
2020-10-07 19:56:08 -0500

    Remove 'ZenUnitPy/ZenUnitPyTests' from start of where ZenUnitPy CoberturaCodeCoverageResults_ZenUnitPyTests.xml is written

--- ZenUnitPy/ZenUnitPyTests/RunAllWithCoverage.py
+++ ZenUnitPy/ZenUnitPyTests/RunAllWithCoverage.py
@@ -4,5 +4,5 @@ from ZenUnitPy import Process
 
 Process.run('coverage3 run --branch --omit="/usr/*" RunAll.py')
 Process.run('coverage3 report --fail-under=100 --show-missing')
-Process.run('coverage3 xml -o ZenUnitPy/ZenUnitPyTests/CoberturaCodeCoverageResults_ZenUnitPyTests.xml')
+Process.run('coverage3 xml -o CoberturaCodeCoverageResults_ZenUnitPyTests.xml')
 Process.run('coverage3 html')
