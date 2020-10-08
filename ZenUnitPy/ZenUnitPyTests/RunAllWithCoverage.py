f82028e099dae4201ea1da04a5b8db2588607cd4
Neil Justice <njjustice@gmail.com>
2020-10-07 19:52:07 -0500

    Write ZenUnitPy coverage results to ZenUnitPy/ZenUnitPyTests/CoberturaCodeCoverageResults_ZenUnitPyTests.xml

--- ZenUnitPy/ZenUnitPyTests/RunAllWithCoverage.py
+++ ZenUnitPy/ZenUnitPyTests/RunAllWithCoverage.py
@@ -3,6 +3,6 @@ sys.path.append('..') # Jenkins
 from ZenUnitPy import Process
 
 Process.run('coverage3 run --branch --omit="/usr/*" RunAll.py')
-Process.run('coverage3 report')
+Process.run('coverage3 report --fail-under=100 --show-missing')
+Process.run('coverage3 xml -o ZenUnitPy/ZenUnitPyTests/CoberturaCodeCoverageResults_ZenUnitPyTests.xml')
 Process.run('coverage3 html')
-Process.run('coverage3 xml')
