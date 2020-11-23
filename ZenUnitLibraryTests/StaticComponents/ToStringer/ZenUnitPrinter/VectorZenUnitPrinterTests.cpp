834e026f4a0bbfda4dd9d54a248241f1e76a5967
Neil Justice <njjustice@gmail.com>
2020-11-22 20:26:17 -0600

    Add UserType tests to VectorOfUserTypesZenUnitPrinterTests

--- ZenUnitLibraryTests/StaticComponents/ToStringer/ZenUnitPrinter/VectorZenUnitPrinterTests.cpp
+++ ZenUnitLibraryTests/StaticComponents/ToStringer/ZenUnitPrinter/VectorZenUnitPrinterTests.cpp
@@ -20,8 +20,8 @@ namespace ZenUnit
       //
       const string vectorAsString = _oss.str();
       ARE_EQUAL(_expectedVectorTypePrefix + "0):" + TestUtil::NewlineConcat("",
-"{",
-"}"), vectorAsString);
+         "{",
+         "}"), vectorAsString);
    }
 
    TEST(ZenUnitPrinterPrint_VectorHasOneElement_PrintsElement)
@@ -32,9 +32,9 @@ namespace ZenUnit
       //
       const string vectorAsString = _oss.str();
       const string expectedVectorAsString = _expectedVectorTypePrefix + "1):" + TestUtil::NewlineConcat("",
-"{",
-"   1",
-"}");
+         "{",
+         "   1",
+         "}");
       ARE_EQUAL(expectedVectorAsString, vectorAsString);
    }
 
@@ -46,22 +46,97 @@ namespace ZenUnit
       //
       const string vectorAsString = _oss.str();
       const string expectedVectorAsString = _expectedVectorTypePrefix + "11):" + TestUtil::NewlineConcat("",
-"{",
-"   1,",
-"   2,",
-"   3,",
-"   4,",
-"   5,",
-"   6,",
-"   7,",
-"   8,",
-"   9,",
-"   10,",
-"   11",
-"}");
+         "{",
+         "   1,",
+         "   2,",
+         "   3,",
+         "   4,",
+         "   5,",
+         "   6,",
+         "   7,",
+         "   8,",
+         "   9,",
+         "   10,",
+         "   11",
+         "}");
       ARE_EQUAL(expectedVectorAsString, vectorAsString);
    }
 
    RUN_TEMPLATE_TESTS(VectorZenUnitPrinterTests, int)
    THEN_RUN_TEMPLATE_TESTS(VectorZenUnitPrinterTests, unsigned long long)
+
+
+   TESTS(VectorOfUserTypesZenUnitPrinterTests)
+   AFACT(ZenUnitPrinterPrint_VectorIsEmpty_PrintsEmptyBrackets)
+   AFACT(ZenUnitPrinterPrint_VectorHasOneElement_PrintsElement)
+   AFACT(ZenUnitPrinterPrint_VectorHasElevenElements_PrintsAllElevenElements)
+   EVIDENCE
+
+   const string _expectedVectorTypePrefix = "std::vector<UserType> (size ";
+   ostringstream _oss;
+
+   TEST(ZenUnitPrinterPrint_VectorIsEmpty_PrintsEmptyBrackets)
+   {
+      const vector<UserType> vec;
+      //
+      ZenUnit::Printer<vector<UserType>>::Print(_oss, vec);
+      //
+      const string vectorAsString = _oss.str();
+      ARE_EQUAL(_expectedVectorTypePrefix + "0):" + TestUtil::NewlineConcat("",
+         "{",
+         "}"), vectorAsString);
+   }
+
+   TEST(ZenUnitPrinterPrint_VectorHasOneElement_PrintsElement)
+   {
+      const vector<UserType> vec = { UserType(1) };
+      //
+      ZenUnit::Printer<vector<UserType>>::Print(_oss, vec);
+      //
+      const string vectorAsString = _oss.str();
+      const string expectedVectorAsString = _expectedVectorTypePrefix + "1):" + TestUtil::NewlineConcat("",
+         "{",
+         "   UserType@1",
+         "}");
+      ARE_EQUAL(expectedVectorAsString, vectorAsString);
+   }
+
+   TEST(ZenUnitPrinterPrint_VectorHasElevenElements_PrintsAllElevenElements)
+   {
+      const vector<UserType> vec =
+      {
+         UserType(1),
+         UserType(2),
+         UserType(3),
+         UserType(4),
+         UserType(5),
+         UserType(6),
+         UserType(7),
+         UserType(8),
+         UserType(9),
+         UserType(10),
+         UserType(11)
+      };
+      //
+      ZenUnit::Printer<vector<UserType>>::Print(_oss, vec);
+      //
+      const string vectorAsString = _oss.str();
+      const string expectedVectorAsString = _expectedVectorTypePrefix + "11):" + TestUtil::NewlineConcat("",
+         "{",
+         "   UserType@1,",
+         "   UserType@2,",
+         "   UserType@3,",
+         "   UserType@4,",
+         "   UserType@5,",
+         "   UserType@6,",
+         "   UserType@7,",
+         "   UserType@8,",
+         "   UserType@9,",
+         "   UserType@10,",
+         "   UserType@11",
+         "}");
+      ARE_EQUAL(expectedVectorAsString, vectorAsString);
+   }
+
+   RUN_TESTS(VectorOfUserTypesZenUnitPrinterTests)
 }
