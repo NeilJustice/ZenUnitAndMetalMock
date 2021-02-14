eb400c448a80eefc06f53fe1463b4abfde9167c6
Neil Justice <njjustice@gmail.com>
2021-02-14 13:19:23 -0600

    Remove CMAKE_INSTALL_PREFIX from JenkinsJobs/Windows/ZenUnitAndMetalMock-Debug.ps1 and JenkinsJobs/Windows/ZenUnitAndMetalMock-Release.ps1

--- JenkinsJobs/Windows/ZenUnitAndMetalMock-Debug.ps1
+++ JenkinsJobs/Windows/ZenUnitAndMetalMock-Debug.ps1
@@ -1,11 +1,10 @@
-param([string]$installFolder = "C:\\")
 Set-PSDebug -Trace 1
 
 $env:PYTHONPATH="ZenUnitPy"
 python.exe ZenUnitPy\ZenUnitPy\BuildZenUnitAndMetalMock.py `
-   --cmake-generator="Visual Studio 16 2019" `
-   --cmake-build-type="Debug" `
-   --cmake-definitions="-DCMAKE_INSTALL_PREFIX=$installFolder"
+	--cmake-generator="Visual Studio 16 2019" `
+	--cmake-build-type="Debug" `
+	--cmake-definitions=""
 
 Set-PSDebug -Trace 0
 exit $LastExitCode
