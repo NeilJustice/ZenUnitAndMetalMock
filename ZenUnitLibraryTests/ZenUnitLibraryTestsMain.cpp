66833f080d871063cc3b00d86cc3bb87acc08d73
Neil Justice <njjustice@gmail.com>
2021-03-07 14:17:05 -0600

    Remove experimental commented-out code from ZenUnitLibraryTestsMain.cpp

--- ZenUnitLibraryTests/ZenUnitLibraryTestsMain.cpp
+++ ZenUnitLibraryTests/ZenUnitLibraryTestsMain.cpp
@@ -1,24 +1,5 @@
 #include "pch.h"
 
-//int main()
-//{
-//   const int lowercaseA = static_cast<int>('a');
-//   const int uppercaseA = static_cast<int>('A');
-//   const int lowercaseZ = static_cast<int>('z');
-//   const int uppercaseZ = static_cast<int>('Z');
-//   for (size_t i = 65; i <= 90; ++i)
-//   {
-//      const char c = static_cast<char>(i);
-//      cout << c << '\n';
-//   }
-//   cout << '\n';
-//   for (size_t i = 97; i <= 122; ++i)
-//   {
-//      const char c = static_cast<char>(i);
-//      cout << c << '\n';
-//   }
-//}
-
 int main(int argc, char* argv[])
 {
    ZenUnit::globalZenUnitMode.selfTest = true;
