edba67d37d86e6d5f9e4b1551229728d6ed50e25
Neil Justice <njjustice@gmail.com>
2021-03-13 14:20:57 -0600

    Update JenkinsJobs/Windows/ZenUnitAndMetalMock-Cppcheck.ps1

--- JenkinsJobs/Windows/ZenUnitAndMetalMock-Cppcheck.ps1
+++ JenkinsJobs/Windows/ZenUnitAndMetalMock-Cppcheck.ps1
@@ -1,11 +1,41 @@
 Set-PSDebug -Trace 1
 
 cppcheck.exe `
+	--cppcheck-build-dir=Cppcheck `
    --enable=all `
+   --suppressions-list=CppcheckSuppressions.txt `
+   -DCOMMA `
+   -DAFACT `
+   -DFACTS `
    -DTEST `
    -DTESTS `
+   -DTEST1X1 `
+   -DTEST2X2 `
+   -DTEST3X3 `
+   -DTEST4X4 `
+   -DTEST5X5 `
+   -DTEST6X6 `
+   -DTEST7X7 `
+   -DTEST8X8 `
+   -DTEST9X9 `
+   -DTEST10X10 `
+   -DRUN_TESTS `
+   -DRUN_TEMPLATE_TESTS `
+   -DMETALMOCK_NONVOID0_FREE `
+   -DMETALMOCK_NONVOID0_STATIC `
+   -DMETALMOCK_NONVOID1_FREE `
+   -DMETALMOCK_NONVOID1_STATIC `
+   -DMETALMOCK_NONVOID2_STATIC `
+   -DMETALMOCK_NONVOID3_CONST `
+   -DMETALMOCK_NONVOID5_FREE `
+   -DMETALMOCK_VOID0_FREE `
+   -DMETALMOCK_VOID1_FREE `
+   -DMETALMOCK_VOID2_FREE `
+   -D_WIN32 `
    -I . `
    -j 64 `
+   --output-file=cppcheck_results.txt `
+   --error-exitcode=1 `
    .
 
 Set-PSDebug -Trace 0
