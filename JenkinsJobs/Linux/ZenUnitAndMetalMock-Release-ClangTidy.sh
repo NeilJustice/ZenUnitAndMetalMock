a3711104a006746fbc046fe48f7b590c460ea5a3
Neil Justice <njjustice@gmail.com>
2021-03-15 13:15:15 -0500

    Update Linux Jenkins job scripts

--- JenkinsJobs/Linux/ZenUnitAndMetalMock-Release-ClangTidy.sh
+++ JenkinsJobs/Linux/ZenUnitAndMetalMock-Release-ClangTidy.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -ev
 
-CXX=/usr/bin/clang++ cmake -H. -BRelease -GNinja -DCMAKE_BUILD_TYPE=Release -DClangTidyMode=ON
+export CXX=/usr/bin/clang++
+cmake -H. -BRelease -GNinja -DCMAKE_BUILD_TYPE=Release -DClangTidyMode=ON
 (cd Release; time ninja clang-tidy)
