d411bbb421923f7a55d97ccf3d2b53846f1e1324
Neil Justice <njjustice@gmail.com>
2021-04-14 09:32:28 -0500

    chmod u+x JenkinsJobs/Linux/ZenUnitAndMetalMock-ZenUnitPy-Mypy-Flake8-Pylint-SonarQube.sh

--- ZenUnitPy/ZenUnitPyTests/RunAllWithCoverage.py
+++ ZenUnitPy/ZenUnitPyTests/RunAllWithCoverage.py
@@ -4,4 +4,4 @@ from ZenUnitPy import Python
 if os.getcwd().endswith('ZenUnitPyTests'):
    os.chdir('..')
 
-Python.run_all_with_coverage(testsProjectName='ZenUnitPyTests', omitPattern='/usr/*')
+Python.run_all_with_coverage(testsProjectName='ZenUnitPyTests', omitPattern='/usr/*,/home/*/.local/lib/python3.9/site-packages/*')
