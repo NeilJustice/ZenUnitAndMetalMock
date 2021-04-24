5610ca48aa6f0e69ece6c580d3f7149e95fab38b
Neil Justice <njjustice@gmail.com>
2021-04-24 14:11:21 -0500

    Update JenkinsJobs/Linux/ZenUnitAndMetalMock-Debug-ZenUnitLibraryTests-Callgrind.sh to build ZenUnitLibraryTests before running ZenUnitLibraryTests with callgrind

--- JenkinsJobs/Linux/ZenUnitAndMetalMock-Debug-ZenUnitLibraryTests-Callgrind.sh
+++ JenkinsJobs/Linux/ZenUnitAndMetalMock-Debug-ZenUnitLibraryTests-Callgrind.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-set -e
+set -ev
 
+export PYTHONPATH=ZenUnitPy
+export CXX=/usr/bin/clang++
+python ZenUnitPy/ZenUnitPy/BuildZenUnitAndMetalMock.py --cmake-generator=Ninja --cmake-build-type=Debug --cmake-definitions=""
 time valgrind --tool=callgrind --cache-sim=yes --branch-sim=yes Debug/ZenUnitLibraryTests/ZenUnitLibraryTests
 
