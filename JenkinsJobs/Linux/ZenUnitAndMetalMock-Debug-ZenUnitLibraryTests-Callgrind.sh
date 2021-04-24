414c6955dc82c84a204139badd35f1526529be70
Neil Justice <njjustice@gmail.com>
2021-04-24 14:15:30 -0500

    Update JenkinsJobs/Linux/ZenUnitAndMetalMock-Debug-ZenUnitLibraryTests-Callgrind.sh to set --random-seed=0

--- JenkinsJobs/Linux/ZenUnitAndMetalMock-Debug-ZenUnitLibraryTests-Callgrind.sh
+++ JenkinsJobs/Linux/ZenUnitAndMetalMock-Debug-ZenUnitLibraryTests-Callgrind.sh
@@ -4,5 +4,5 @@ set -ev
 export PYTHONPATH=ZenUnitPy
 export CXX=/usr/bin/clang++
 python ZenUnitPy/ZenUnitPy/BuildZenUnitAndMetalMock.py --cmake-generator=Ninja --cmake-build-type=Debug --cmake-definitions=""
-time valgrind --tool=callgrind --cache-sim=yes --branch-sim=yes Debug/ZenUnitLibraryTests/ZenUnitLibraryTests
+time valgrind --tool=callgrind --cache-sim=yes --branch-sim=yes Debug/ZenUnitLibraryTests/ZenUnitLibraryTests --random-seed=0
 
