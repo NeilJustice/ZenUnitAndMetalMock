7ffe5a5ac467077dc2ed1ccf42123daae615ec6a
Neil Justice <njjustice@gmail.com>
2021-04-24 15:07:03 -0500

    Rename JenkinsJobs/Linux/ZenUnitAndMetalMock-Debug-ZenUnitLibraryTests-Callgrind.sh -> JenkinsJobs/Linux/ZenUnitAndMetalMock-Debug-ZenUnitUtilsAndAssertionTests-Callgrind.sh

--- JenkinsJobs/Linux/ZenUnitAndMetalMock-Debug-ZenUnitUtilsAndAssertionTests-Callgrind.sh
+++ JenkinsJobs/Linux/ZenUnitAndMetalMock-Debug-ZenUnitUtilsAndAssertionTests-Callgrind.sh
@@ -4,5 +4,5 @@ set -ev
 export PYTHONPATH=ZenUnitPy
 export CXX=/usr/bin/clang++
 python ZenUnitPy/ZenUnitPy/BuildZenUnitAndMetalMock.py --cmake-generator=Ninja --cmake-build-type=Debug --cmake-definitions=""
-time valgrind --tool=callgrind --cache-sim=yes --branch-sim=yes Debug/ZenUnitLibraryTests/ZenUnitLibraryTests --random-seed=0
+time valgrind --tool=callgrind --cache-sim=yes --branch-sim=yes Debug/ZenUnitUtilsAndAssertionTests/ZenUnitUtilsAndAssertionTests --random-seed=0
 
