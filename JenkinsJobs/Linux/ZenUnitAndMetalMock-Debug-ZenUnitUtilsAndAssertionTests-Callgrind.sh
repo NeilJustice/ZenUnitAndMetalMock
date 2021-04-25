e7af2b75421be125bcc5259a48e14683c68d2752
Neil Justice <njjustice@gmail.com>
2021-04-24 19:46:19 -0500

    Update JenkinsJobs/Linux/ZenUnitAndMetalMock-Debug-ZenUnitUtilsAndAssertionTests-Callgrind.sh to write callgrind_results.txt

--- JenkinsJobs/Linux/ZenUnitAndMetalMock-Debug-ZenUnitUtilsAndAssertionTests-Callgrind.sh
+++ JenkinsJobs/Linux/ZenUnitAndMetalMock-Debug-ZenUnitUtilsAndAssertionTests-Callgrind.sh
@@ -4,5 +4,6 @@ set -ev
 export PYTHONPATH=ZenUnitPy
 export CXX=/usr/bin/clang++
 python ZenUnitPy/ZenUnitPy/BuildZenUnitAndMetalMock.py --cmake-generator=Ninja --cmake-build-type=Debug --cmake-definitions=""
-time valgrind --tool=callgrind --cache-sim=yes --branch-sim=yes Debug/ZenUnitUtilsAndAssertionTests/ZenUnitUtilsAndAssertionTests --random-seed=0
+time valgrind --tool=callgrind --cache-sim=yes --branch-sim=yes Debug/ZenUnitUtilsAndAssertionTests/ZenUnitUtilsAndAssertionTests --random-seed=0 2> callgrind_results.txt
+cat callgrind_results.txt
 
