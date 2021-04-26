0a2f9eb3c442d5de57c0703833dd7b0dbdf52cbb
Neil Justice <njjustice@gmail.com>
2021-04-26 12:39:51 -0500

    Add JenkinsJobs/Linux/ZenUnitAndMetalMock-Debug-ZenUnitUtilsAndAssertionTests-Heaptrack.sh

---
+++ JenkinsJobs/Linux/ZenUnitAndMetalMock-Debug-ZenUnitUtilsAndAssertionTests-Heaptrack.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+set -ev
+
+export PYTHONPATH=ZenUnitPy
+export CXX=/usr/bin/clang++
+python ZenUnitPy/ZenUnitPy/BuildZenUnitAndMetalMock.py --cmake-generator=Ninja --cmake-build-type=Debug --cmake-definitions=""
+heaptrack Debug/ZenUnitUtilsAndAssertionTests/ZenUnitUtilsAndAssertionTests --random-seed=0
