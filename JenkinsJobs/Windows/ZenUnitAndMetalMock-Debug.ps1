f2a16fa0d17f33a6fcc62399a39798c9f176d1cb
Neil Justice <njjustice@gmail.com>
2021-06-04 20:47:41 -0500

    Add CMake variable FastWindowsDebugBuildMode to optionally build without symbols for faster build speed

--- JenkinsJobs/Windows/ZenUnitAndMetalMock-Debug.ps1
+++ JenkinsJobs/Windows/ZenUnitAndMetalMock-Debug.ps1
@@ -4,7 +4,7 @@ $env:PYTHONPATH="."
 python.exe ZenUnitDevOpsPython\BuildAndTestZenUnitAndMetalMock.py `
    --cmake-generator="Visual Studio 16 2019" `
    --cmake-build-type="Debug" `
-   --cmake-definitions=""
+   --cmake-definitions="-DFastWindowsDebugBuildMode=ON"
 
 Set-PSDebug -Trace 0
 exit $LastExitCode
