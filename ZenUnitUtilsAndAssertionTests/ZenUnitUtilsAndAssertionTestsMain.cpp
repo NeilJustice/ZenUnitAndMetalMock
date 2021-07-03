08cfcd2834e7a3c37a248a811cb2c1e500005554
Neil Justice <njjustice@gmail.com>
2021-07-02 22:44:49 -0500

    Add // LCOV_EXCL_LINE lines to skipped test functions in ZenUnitUtilsAndAssertionTestsMain.cpp

--- ZenUnitUtilsAndAssertionTests/ZenUnitUtilsAndAssertionTestsMain.cpp
+++ ZenUnitUtilsAndAssertionTests/ZenUnitUtilsAndAssertionTestsMain.cpp
@@ -9,20 +9,25 @@ SKIPAFACT(SkippedNormalTest, Reason)
 SKIPFACTS(SkippedTest1X1, Reason)
 SKIPFACTS(SkippedTest2X2, Reason)
 EVIDENCE
+
 TEST(NonSkippedTest) {}
 TEST1X1(NonSkipped1X1Test, bool, true, false) {}
-TEST(SkippedNormalTest) {}
-TEST1X1(SkippedTest1X1, int, 0) {}
-TEST2X2(SkippedTest2X2, int, int, 0, 0) {}
+TEST(SkippedNormalTest) {} // LCOV_EXCL_LINE
+TEST1X1(SkippedTest1X1, int, 0) {} // LCOV_EXCL_LINE
+TEST2X2(SkippedTest2X2, int, int, 0, 0) {} // LCOV_EXCL_LINE
+
 RUN_TESTS(SkippedTestsTestClass)
+
 
 TESTS(SkippedTestClassA)
 EVIDENCE
 SKIP_TESTS(SkippedTestClassA, Reason)
 
+
 TESTS(SkippedTestClassB)
 EVIDENCE
 SKIP_TESTS(SkippedTestClassB, Reason)
+
 
 template<typename T>
 TEMPLATE_TESTS(SkippedTemplateTestClassA, T)
@@ -30,11 +35,13 @@ EVIDENCE
 SKIP_TEMPLATE_TESTS(SkippedTemplateTestClassA, Reason, int)
 THEN_SKIP_TEMPLATE_TESTS(SkippedTemplateTestClassA, Reason, double)
 
+
 template<template<typename...> class MapType>
 TEMPLATE_TESTS(SkippedTemplateTestClassB, MapType)
 EVIDENCE
 RUN_TEMPLATE_TESTS(SkippedTemplateTestClassB, map)
 THEN_SKIP_TEMPLATE_TESTS(SkippedTemplateTestClassB, Reason, unordered_map)
+
 
 int main(int argc, char* argv[])
 {
