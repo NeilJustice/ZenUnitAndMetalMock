d9cac580c70246bc9ba4726fee45dbd5c241ff0e
Neil Justice <njjustice@gmail.com>
2021-09-26 22:18:24 -0500

    Update llvm-profdata-cov.py

--- llvm-profdata-cov.py
+++ llvm-profdata-cov.py
@@ -1,20 +1,11 @@
 import subprocess
 import shlex
 
-# subprocess.call(shlex.split("""llvm-profdata merge
-# Debug/ZenUnitLibraryTests/coverage.profraw -sparse -o coverage.profdata"""))
-
-# subprocess.call(shlex.split("""llvm-cov report
-# Debug/ZenUnitLibraryTests/ZenUnitLibraryTests -instr-profile=coverage.profdata"""))
-
-# subprocess.call(shlex.split("""llvm-cov show
-# Debug/ZenUnitLibraryTests/ZenUnitLibraryTests -instr-profile=coverage.profdata -project-title=ZenUnit -format=html -output-dir=covhtml -show-instantiation-summary -show-line-counts-or-regions -Xdemangler c++filt"""))
-
 subprocess.call(shlex.split("""llvm-profdata merge
-  Debug/MetalMockTests/coverage.profraw -sparse -o coverage.profdata"""))
+Debug/ZenUnitLibraryTests/coverage.profraw -sparse -o coverage.profdata"""))
 
 subprocess.call(shlex.split("""llvm-cov report
-  Debug/MetalMockTests/MetalMockTests -instr-profile=coverage.profdata"""))
+Debug/ZenUnitLibraryTests/ZenUnitLibraryTests -instr-profile=coverage.profdata"""))
 
 subprocess.call(shlex.split("""llvm-cov show
-  Debug/MetalMockTests/MetalMockTests -instr-profile=coverage.profdata -project-title=ZenUnit -format=html -output-dir=covhtml -show-instantiation-summary -show-line-counts-or-regions -Xdemangler c++filt"""))
+Debug/ZenUnitLibraryTests/ZenUnitLibraryTests -instr-profile=coverage.profdata -project-title=ZenUnit -format=html -output-dir=covhtml -show-instantiation-summary -show-line-counts-or-regions -Xdemangler c++filt"""))
