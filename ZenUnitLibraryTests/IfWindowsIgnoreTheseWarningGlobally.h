0ca170f0399e402cfbbb4aa60b3054c5921fbe69
Neil Justice <njjustice@gmail.com>
2022-11-10 09:53:50 -0600

    Update suppressed warnings in ZenUnitLibraryTests/IfWindowsIgnoreTheseWarningGlobally.h

--- ZenUnitLibraryTests/IfWindowsIgnoreTheseWarningGlobally.h
+++ ZenUnitLibraryTests/IfWindowsIgnoreTheseWarningGlobally.h
@@ -2,6 +2,7 @@
 
 #if defined _WIN32
 #pragma warning(disable: 4371) // layout of class may have changed from a previous version of the compiler due to better packing of member
+#pragma warning(disable: 4514) // unreferenced inline function has been removed
 #pragma warning(disable: 4625) // copy constructor was implicitly defined as deleted
 #pragma warning(disable: 4626) // assignment operator was implicitly defined as deleted
 #pragma warning(disable: 4710) // function not inlined
@@ -11,4 +12,6 @@
 #pragma warning(disable: 5027) // move assignment operator was implicitly defined as deleted
 #pragma warning(disable: 5039) // pointer or reference to potentially throwing function passed to extern C function under - EHc. Undefined behavior may occur if this function throws an exception
 #pragma warning(disable: 5045) // Compiler will insert Spectre mitigation for memory load if / Qspectre switch specified
+#pragma warning(disable: 5262) // implicit fall-through occurs here
+#pragma warning(disable: 5264) // variable is not used
 #endif
