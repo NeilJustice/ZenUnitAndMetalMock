25428f04bd839ff731d55c1435893bebb2e0b4ea
Neil Justice <njjustice@gmail.com>
2022-12-31 18:53:39 -0600

    Update JenkinsJobs/Windows/ZenUnitAndMetalMock-VTuneHotspots-ZenUnitLibraryTests-Debug.ps1 and JenkinsJobs/Windows/ZenUnitAndMetalMock-VTuneThreading-ZenUnitLibraryTests-Debug.ps1

--- JenkinsJobs/Windows/ZenUnitAndMetalMock-VTuneHotspots-ZenUnitLibraryTests-Debug.ps1
+++ JenkinsJobs/Windows/ZenUnitAndMetalMock-VTuneHotspots-ZenUnitLibraryTests-Debug.ps1
@@ -1,3 +1,10 @@
+$env:PYTHONPATH="."
+python.exe -u ZenUnitDevOpsPython\BuildAndTestZenUnitAndMetalMock.py `
+   --cmake-generator="Visual Studio 17 2022" `
+   --cmake-build-type="Debug" `
+   --cmake-definitions=""
+if ($LastExitCode -ne 0) { exit $LastExitCode }
+
 AcceliTune.exe collect-hotspots `
    --app-working-dir=ZenUnitLibraryTests\Debug `
    --program-name=ZenUnitLibraryTests.exe `
