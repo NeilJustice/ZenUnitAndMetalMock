070295cfafc4fb57ee72ce39688b46e679cffc03
Neil Justice <njjustice@gmail.com>
2022-12-31 20:11:11 -0600

    Add '--test-runs=10' to JenkinsJobs/Windows/ZenUnitAndMetalMock-VTuneHotspots-ZenUnitLibraryTests-Debug.ps1 and JenkinsJobs/Windows/ZenUnitAndMetalMock-VTuneThreading-ZenUnitLibraryTests-Debug.ps1

--- JenkinsJobs/Windows/ZenUnitAndMetalMock-VTuneHotspots-ZenUnitLibraryTests-Debug.ps1
+++ JenkinsJobs/Windows/ZenUnitAndMetalMock-VTuneHotspots-ZenUnitLibraryTests-Debug.ps1
@@ -8,6 +8,6 @@ if ($LastExitCode -ne 0) { exit $LastExitCode }
 AcceliTune.exe collect-hotspots `
    --app-working-dir=ZenUnitLibraryTests\Debug `
    --program-name=ZenUnitLibraryTests.exe `
-   --program-args="" `
+   --program-args="--test-runs=10" `
    --results-folder=C:\VTuneProfilingResults\ZenUnitLibraryTests_Hotspots `
    --overwrite-results-folder
