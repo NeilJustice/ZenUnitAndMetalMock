311bf639b67721efe21056d2af5e2011bcdb11a9
Neil Justice <njjustice@gmail.com>
2022-12-31 18:43:37 -0600

    Add JenkinsJobs/Windows/ZenUnitAndMetalMock-VTuneHotspots-ZenUnitLibraryTests-Debug.ps1 and JenkinsJobs/Windows/ZenUnitAndMetalMock-VTuneThreading-ZenUnitLibraryTests-Debug.ps1

---
+++ JenkinsJobs/Windows/ZenUnitAndMetalMock-VTuneHotspots-ZenUnitLibraryTests-Debug.ps1
@@ -0,0 +1,6 @@
+AcceliTune.exe collect-hotspots `
+   --app-working-dir=ZenUnitLibraryTests\Debug `
+   --program-name=ZenUnitLibraryTests.exe `
+   --program-args="" `
+   --results-folder=C:\VTuneProfilingResults\ZenUnitLibraryTests_Hotspots `
+   --overwrite-results-folder
---
+++ JenkinsJobs/Windows/ZenUnitAndMetalMock-VTuneThreading-ZenUnitLibraryTests-Debug.ps1
@@ -0,0 +1,6 @@
+AcceliTune.exe collect-threading `
+   --app-working-dir=ZenUnitLibraryTests\Debug `
+   --program-name=ZenUnitLibraryTests.exe `
+   --program-args="" `
+   --results-folder=C:\VTuneProfilingResults\ZenUnitLibraryTests_Threading `
+   --overwrite-results-folder
