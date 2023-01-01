cf72d217a1d49f94b29da0cbde9fb3508ff5bf34
Neil Justice <njjustice@gmail.com>
2023-01-01 14:04:43 -0600

    Update JenkinsJobs/Windows/ZenUnitAndMetalMock-VSDiagnostics-ZenUnitLibraryTests-Debug.ps1

--- JenkinsJobs/Windows/ZenUnitAndMetalMock-VSDiagnostics-ZenUnitLibraryTests-Debug.ps1
+++ JenkinsJobs/Windows/ZenUnitAndMetalMock-VSDiagnostics-ZenUnitLibraryTests-Debug.ps1
@@ -5,11 +5,11 @@ python.exe -u ZenUnitDevOpsPython\BuildAndTestZenUnitAndMetalMock.py `
    --cmake-definitions=""
 if ($LastExitCode -ne 0) { exit $LastExitCode }
 
-VSDiagnostics.exe start 5 `
-   /launch:ZenUnitLibraryTests\Debug\ZenUnitLibraryTests.exe `
+VSDiagnostics.exe start 1 `
+   /launch:"ZenUnitLibraryTests\Debug\ZenUnitLibraryTests.exe" `
    /launchArgs:"--test-runs=10" `
-   /loadConfig:JenkinsJobs\Windows\VSDiagnosticsCPUUsage.json
+   /loadConfig:"JenkinsJobs\Windows\VSDiagnosticsCPUUsage.json"
 if ($LastExitCode -ne 0) { exit $LastExitCode }
 sleep 30
-VSDiagnostics.exe stop 5 /output:JenkinsJobs\Windows\ZenUnitLibraryTests-Debug.diagsession
+VSDiagnostics.exe stop 1 /output:"C:\PerformanceProfilingResults\VSDiagnostics\ZenUnitLibraryTests-Debug.diagsession"
 exit $LastExitCode
