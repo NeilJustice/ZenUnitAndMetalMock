10845fd659dd59ab5dbf1cdc79b251ce91401925
Neil Justice <njjustice@gmail.com>
2023-05-19 10:05:05 -0500

    Update AcceliTune.exe args in JenkinsJobs/Windows/ZenUnitAndMetalMock-VTuneThreading-ZenUnitLibraryTests-Debug.ps1

--- JenkinsJobs/Windows/ZenUnitAndMetalMock-VTuneThreading-ZenUnitLibraryTests-Debug.ps1
+++ JenkinsJobs/Windows/ZenUnitAndMetalMock-VTuneThreading-ZenUnitLibraryTests-Debug.ps1
@@ -7,7 +7,7 @@ if ($LastExitCode -ne 0) { exit $LastExitCode }
 
 AcceliTune.exe collect-threading `
    --app-working-dir=ZenUnitLibraryTests\Debug `
-   --program-name=ZenUnitLibraryTests.exe `
-   --program-args="--test-runs=10" `
+   --program=ZenUnitLibraryTests.exe `
+   --args="--test-runs=10" `
    --results-folder="C:\PerformanceProfilingResults\VTune\ZenUnitLibraryTests_Threading" `
    --overwrite-results-folder
