6bf497b71ee2a7e8ff328d5be4826b13fec23380
Neil Justice <njjustice@gmail.com>
2023-07-12 21:52:19 -0500

    Add ZenUnitAndMetalMock-VTuneMemory-ZenUnitLibraryTests-Debug.ps1

---
+++ JenkinsJobs/Windows/ZenUnitAndMetalMock-VTuneMemory-ZenUnitLibraryTests-Debug.ps1
@@ -0,0 +1,13 @@
+$env:PYTHONPATH="."
+python.exe -u ZenUnitDevOpsPython\BuildAndTestZenUnitAndMetalMock.py `
+   --cmake-generator="Visual Studio 17 2022" `
+   --cmake-build-type="Debug" `
+   --cmake-definitions=""
+if ($LastExitCode -ne 0) { exit $LastExitCode }
+
+AcceliTune.exe collect-memory-access `
+   --app-working-dir=ZenUnitLibraryTests\Debug `
+   --program=ZenUnitLibraryTests.exe `
+   --args="--test-runs=10" `
+   --results-folder="C:\PerformanceProfilingResults\VTune\ZenUnitLibraryTests_Memory " `
+   --overwrite-results-folder
