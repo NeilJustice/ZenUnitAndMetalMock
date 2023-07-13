76a8aae8efe1c9124b46b2e315a3f384aeeb11ab
Neil Justice <njjustice@gmail.com>
2023-07-13 12:48:31 -0500

    Add JenkinsJobs/Windows/ZenUnitAndMetalMock-VTuneArchitecture-ZenUnitLibraryTests-Debug.ps1

---
+++ JenkinsJobs/Windows/ZenUnitAndMetalMock-VTuneArchitecture-ZenUnitLibraryTests-Debug.ps1
@@ -0,0 +1,14 @@
+$env:PYTHONPATH="."
+python.exe -u ZenUnitDevOpsPython\BuildAndTestZenUnitAndMetalMock.py `
+   --cmake-generator="Visual Studio 17 2022" `
+   --cmake-build-type="Debug" `
+   --cmake-definitions=""
+if ($LastExitCode -ne 0) { exit $LastExitCode }
+Write-Host
+
+AcceliTune.exe collect-uarch-exploration `
+   --app-working-dir=ZenUnitLibraryTests\Debug `
+   --program=ZenUnitLibraryTests.exe `
+   --args="--test-runs=10" `
+   --results-folder="C:\PerformanceProfilingResults\VTune\ZenUnitLibraryTests_Architecture" `
+   --overwrite-results-folder
