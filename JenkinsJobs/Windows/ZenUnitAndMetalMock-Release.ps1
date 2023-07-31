6284f80488f778d519d2828ac106661ffda7789b
Neil Justice <njjustice@gmail.com>
2023-07-30 21:56:29 -0500

    Remove JenkinsJobs/Windows/ZenUnitAndMetalMock-Release.ps1

--- JenkinsJobs/Windows/ZenUnitAndMetalMock-Release.ps1
+++
@@ -1,6 +0,0 @@
-$env:PYTHONPATH="."
-python.exe -u ZenUnitDevOpsPython\BuildAndTestZenUnitAndMetalMock.py `
-   --cmake-generator="Visual Studio 17 2022" `
-   --cmake-build-type="Release" `
-   --cmake-definitions=""
-exit $LastExitCode
