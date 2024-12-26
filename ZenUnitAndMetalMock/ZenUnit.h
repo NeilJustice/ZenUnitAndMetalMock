ab202c7b33c65c45581e883db70eaa47244c82f3
Neil Justice <njjustice@gmail.com>
2024-12-26 11:24:37 -0500

    Fix two const-themed Linux Cppcheck issues

--- ZenUnitAndMetalMock/ZenUnit.h
+++ ZenUnitAndMetalMock/ZenUnit.h
@@ -1196,9 +1196,8 @@ namespace ZenUnit
       {
          const std::chrono::time_point<std::chrono::system_clock> nowTimePoint = std::chrono::system_clock::now();
 #if defined __linux__ || defined __APPLE__
-         tm* tmNow = nullptr;
-         long nowTimeT = std::chrono::system_clock::to_time_t(nowTimePoint);
-         tmNow = localtime(&nowTimeT);
+         const long nowTimeT = std::chrono::system_clock::to_time_t(nowTimePoint);
+         const tm* const tmNow = localtime(&nowTimeT);
          return *tmNow;
 #elif defined _WIN32
          const __time64_t nowTimeT = std::chrono::system_clock::to_time_t(nowTimePoint);
@@ -4865,7 +4864,7 @@ namespace ZenUnit
       virtual std::string UserName() const
       {
          const uid_t uidValue = geteuid();
-         struct passwd* const passwdValue = getpwuid(uidValue);
+         struct const passwd* const passwdValue = getpwuid(uidValue);
          std::string userName(passwdValue->pw_name);
          return userName;
       }
