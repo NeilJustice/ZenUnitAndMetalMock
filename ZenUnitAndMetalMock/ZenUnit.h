75cba3f4d13bc7b981d0631b0c90808c812dbbc1
Neil Justice <njjustice@gmail.com>
2024-12-26 09:56:42 -0500

    Remove line breaks from console write line for 'A test failed in --fail-fast mode...' to work around an lcov or Codecov.io bug which reports this line as uncovered if there are line breaks in it

--- ZenUnitAndMetalMock/ZenUnit.h
+++ ZenUnitAndMetalMock/ZenUnit.h
@@ -5846,11 +5846,8 @@ Fatal Windows C++ Runtime Assertion
          if (zenUnitArgs.failFast && testOutcome != TestOutcome::Success)
          {
             const int exitCode = zenUnitArgs.alwaysExit0 ? 0 : 1;
-            const std::string failFastMessage = String::ConcatValues('\n', 
-               "[ZenUnit] A test failed in --fail-fast mode.\n", 
-               "[ZenUnit] Completed: ", zenUnitArgs.commandLine, '\n', 
-               "[ZenUnit]   TestRun: ", globalZenUnitMode.currentTestRunNumber, " of ", zenUnitArgs.testRuns, '\n', 
-               "[ZenUnit]  ExitCode: ", exitCode);
+            // One line here instead of line breaks to work around an lcov or Codecov.io bug which reports this line as uncovered if there are line breaks in it.
+            const std::string failFastMessage = String::ConcatValues('\n', "[ZenUnit] A test failed in --fail-fast mode.\n", "[ZenUnit] Completed: ", zenUnitArgs.commandLine, '\n', "[ZenUnit]   TestRun: ", globalZenUnitMode.currentTestRunNumber, " of ", zenUnitArgs.testRuns, '\n', "[ZenUnit]  ExitCode: ", exitCode);
             _console->WriteLineAndExit(failFastMessage, exitCode);
          }
       }
