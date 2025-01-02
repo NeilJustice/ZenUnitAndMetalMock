f3f4a6fdfac04d9fbfe3f690cd51c1b7ee1f14ea
Neil Justice <njjustice@gmail.com>
2025-01-02 15:11:48 -0500

    Replace #else with #elifdef __linux__ in sizeof static assertions in ObjectSizeStaticAssertions.cpp

--- ZenUnitUtilsAndAssertionTests/Testing/ObjectSizeStaticAssertions.cpp
+++ ZenUnitUtilsAndAssertionTests/Testing/ObjectSizeStaticAssertions.cpp
@@ -2,103 +2,103 @@
 
 namespace ZenUnit
 {
-   #if defined(_WIN32)
-      #if defined _DEBUG
+   #ifdef _WIN32
+      #ifdef _DEBUG
          static_assert(sizeof(Anomaly) == 280);
-      #elif NDEBUG
+      #else
          static_assert(sizeof(Anomaly) == 232);
       #endif
-   #else
+   #elifdef __linux__
       static_assert(sizeof(Anomaly) == 216);
    #endif
 
-   #if defined(_WIN32)
-      #if defined _DEBUG
+   #ifdef _WIN32
+      #ifdef _DEBUG
          static_assert(sizeof(AnomalyOrException) == 40);
-      #elif NDEBUG
+      #else
          static_assert(sizeof(AnomalyOrException) == 40);
       #endif
-   #else
+   #elifdef __linux__
       static_assert(sizeof(AnomalyOrException) == 40);
    #endif
 
-   #if defined(_WIN32)
-      #if defined _DEBUG
+   #ifdef _WIN32
+      #ifdef _DEBUG
          static_assert(sizeof(FilePathLineNumber) == 16);
-      #elif NDEBUG
+      #else
          static_assert(sizeof(FilePathLineNumber) == 16);
       #endif
-   #else
+   #elifdef __linux__
       static_assert(sizeof(FilePathLineNumber) == 16);
    #endif
 
-   #if defined(_WIN32)
-      #if defined _DEBUG
+   #ifdef _WIN32
+      #ifdef _DEBUG
          static_assert(sizeof(FullTestName) == 24);
-      #elif NDEBUG
+      #else
          static_assert(sizeof(FullTestName) == 24);
       #endif
-   #else
+   #elifdef __linux__
       static_assert(sizeof(FullTestName) == 24);
    #endif
 
-   #if defined(_WIN32)
-      #if defined _DEBUG
+   #ifdef _WIN32
+      #ifdef _DEBUG
          static_assert(sizeof(Test) == 64);
-      #elif NDEBUG
+      #else
          static_assert(sizeof(Test) == 64);
       #endif
-   #else
+   #elifdef __linux__
       static_assert(sizeof(Test) == 64);
    #endif
 
-   #if defined(_WIN32)
-      #if defined _DEBUG
+   #ifdef _WIN32
+      #ifdef _DEBUG
          static_assert(sizeof(TestClassResult) == 104);
-      #elif NDEBUG
+      #else
          static_assert(sizeof(TestClassResult) == 96);
       #endif
-   #else
+   #elifdef __linux__
       static_assert(sizeof(TestClassResult) == 64);
    #endif
 
-   #if defined(_WIN32)
-      #if defined _DEBUG
+   #ifdef _WIN32
+      #ifdef _DEBUG
          static_assert(sizeof(TestNameFilter) == 96);
-      #elif NDEBUG
+      #else
          static_assert(sizeof(TestNameFilter) == 80);
       #endif
-   #else
+   #elifdef __linux__
       static_assert(sizeof(TestNameFilter) == 80);
    #endif
 
-   #if defined(_WIN32)
-      #if defined _DEBUG
+   #ifdef _WIN32
+      #ifdef _DEBUG
          static_assert(sizeof(TestPhaseResult) == 24);
-      #elif NDEBUG
+      #else
          static_assert(sizeof(TestPhaseResult) == 24);
       #endif
-   #else
+   #elifdef __linux__
       static_assert(sizeof(TestPhaseResult) == 24);
    #endif
 
-   #if defined(_WIN32)
-      #if defined _DEBUG
+   #ifdef _WIN32
+      #ifdef _DEBUG
          static_assert(sizeof(TestResult) == 184);
-      #elif NDEBUG
+      #else
          static_assert(sizeof(TestResult) == 184);
       #endif
-   #else
+   #elifdef __linux__
       static_assert(sizeof(TestResult) == 184);
    #endif
 
-   #if defined(_WIN32)
+   #ifdef _WIN32
       #if defined _DEBUG
          static_assert(sizeof(ZenUnitArgs) == 136);
-      #elif NDEBUG
+      #else
          static_assert(sizeof(ZenUnitArgs) == 112);
       #endif
-   #else
+   #elifdef __linux__
       static_assert(sizeof(ZenUnitArgs) == 112);
    #endif
 }
