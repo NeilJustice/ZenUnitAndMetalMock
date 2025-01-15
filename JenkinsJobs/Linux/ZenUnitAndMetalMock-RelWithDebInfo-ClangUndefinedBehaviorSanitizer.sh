ef3521e0963a580957b8cb4a98cf02107531e27a
Neil Justice <njjustice@gmail.com>
2025-01-14 19:11:34 -0500

    Update JenkinsJobs/Linux/ZenUnitAndMetalMock-RelWithDebInfo-ClangUndefinedBehaviorSanitizer.sh

--- JenkinsJobs/Linux/ZenUnitAndMetalMock-RelWithDebInfo-ClangUndefinedBehaviorSanitizer.sh
+++ JenkinsJobs/Linux/ZenUnitAndMetalMock-RelWithDebInfo-ClangUndefinedBehaviorSanitizer.sh
@@ -3,7 +3,6 @@ set -ev
 
 export CXX=/usr/bin/clang++
 export PYTHONPATH=.
-export UBSAN_OPTIONS=halt_on_error=true
 python -u ZenUnitDevOpsPython/BuildAndTestZenUnitAndMetalMock.py \
    --cmake-build-type=RelWithDebInfo \
-   --cmake-definitions="-DClangUndefinedBehaviorSanitizerMode=ON -DFastLinuxReleaseBuildMode=ON"
+   --cmake-definitions="-DClangUndefinedBehaviorSanitizerMode=ON"
