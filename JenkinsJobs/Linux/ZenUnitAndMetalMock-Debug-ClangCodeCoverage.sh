7a8685c5e72c1fd9f04604d5a887d55813f5bbaa
Neil Justice <njjustice@gmail.com>
2025-06-03 12:10:02 -0500

    JenkinsJobs/Linux/ZenUnitAndMetalMock-Debug-ClangCodeCoverage.sh: Change invocation of CodeCovSidekick.py to C# program LinuxCodeCoverageRunner for measuring code coverage with LLVM

--- JenkinsJobs/Linux/ZenUnitAndMetalMock-Debug-ClangCodeCoverage.sh
+++ JenkinsJobs/Linux/ZenUnitAndMetalMock-Debug-ClangCodeCoverage.sh
@@ -7,4 +7,4 @@ python -u ZenUnitDevOpsPython/BuildAndTestZenUnitAndMetalMock.py \
    --cmake-build-type=Debug \
    --cmake-definitions="-DClangCodeCoverageMode=ON"
 
-python /code/CodeCovSidekick/CodeCovSidekick.py --solution=ZenUnitAndMetalMock --tests-project=ZenUnitUtilsAndAssertionTests
+LinuxCodeCoverageRunner measure-cplusplus-code-coverage --solution=ZenUnitAndMetalMock --tests-project=ZenUnitUtilsAndAssertionTests
