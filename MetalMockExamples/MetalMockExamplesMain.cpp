23ad842f0e856a96fbd89489aed7cf09e642d9ec
Neil Justice <njjustice@gmail.com>
2025-09-20 10:25:29 -0500

    MetalMockExamplesMain.cpp: Change 'const int exitCode' to 'int exitCode'

--- MetalMockExamples/MetalMockExamplesMain.cpp
+++ MetalMockExamples/MetalMockExamplesMain.cpp
@@ -13,6 +13,6 @@ RUN_TESTS(Tests)
 
 int main(int argc, char* argv[])
 {
-   const int exitCode = ZenUnit::RunTests(argc, argv);
+   int exitCode = ZenUnit::RunTests(argc, argv);
    return exitCode;
 }
