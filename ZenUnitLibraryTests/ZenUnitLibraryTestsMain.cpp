0bb09b007d00705f5d6cccd9045960e4b65df4b7
Neil Justice <njjustice@gmail.com>
2025-09-20 10:24:23 -0500

    ZenUnitLibraryTestsMain.cpp: Change 'const int exit' to 'int exitCode'

--- ZenUnitLibraryTests/ZenUnitLibraryTestsMain.cpp
+++ ZenUnitLibraryTests/ZenUnitLibraryTestsMain.cpp
@@ -3,6 +3,6 @@
 int main(int argc, char* argv[])
 {
    ZenUnit::globalZenUnitMode.selfTest = true;
-   const int exitCode = ZenUnit::RunTests(argc, argv);
+   int exitCode = ZenUnit::RunTests(argc, argv);
    return exitCode;
 }
