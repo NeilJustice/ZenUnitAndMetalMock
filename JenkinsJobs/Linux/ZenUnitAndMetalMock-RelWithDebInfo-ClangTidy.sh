c8eff866355946e50bdc93fca0710fd182b775a8
Neil Justice <njjustice@gmail.com>
2025-12-30 14:01:45 -0600

    Remove Linux/ZenUnitAndMetalMock-RelWithDebInfo-ClangTidy.sh

--- JenkinsJobs/Linux/ZenUnitAndMetalMock-RelWithDebInfo-ClangTidy.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -ev
-
-export CXX=/usr/bin/clang++
-cmake -H. -BRelWithDebInfo -GNinja -DCMAKE_BUILD_TYPE=RelWithDebInfo -DClangTidyMode=ON
-(cd RelWithDebInfo; time ninja clang-tidy)
