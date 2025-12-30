536950c9e3e4ee3c0a8d03c4ccce9d8e578bed3a
Neil Justice <njjustice@gmail.com>
2025-12-29 18:34:27 -0600

    Remove GCC Jenkins job scripts

--- JenkinsJobs/Linux/ZenUnitAndMetalMock-RelWithDebInfo-GCC.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -ev
-
-export CXX=/usr/bin/g++
-export PYTHONPATH=ZenUnitDevOpsPython
-python -u ZenUnitDevOpsPython/ZenUnitDevOpsPython/BuildAndTestZenUnitAndMetalMock.py \
-   --cmake-build-type=RelWithDebInfo \
-   --cmake-definitions="-DFastLinuxReleaseBuildMode=ON"
