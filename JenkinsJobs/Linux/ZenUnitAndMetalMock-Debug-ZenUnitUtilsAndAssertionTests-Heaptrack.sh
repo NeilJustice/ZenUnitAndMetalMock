1355ae13d1174bea69eba08185cbb4a864443bc8
Neil Justice <njjustice@gmail.com>
2026-07-05 12:30:48 -0500

    Delete JenkinsJobs/Linux/ZenUnitAndMetalMock-Debug-ZenUnitUtilsAndAssertionTests-Heaptrack.sh

--- JenkinsJobs/Linux/ZenUnitAndMetalMock-Debug-ZenUnitUtilsAndAssertionTests-Heaptrack.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -ev
-
-export PYTHONPATH=ZenUnitDevOpsPython
-export CXX=/usr/bin/clang++
-python -u ZenUnitDevOpsPython/ZenUnitDevOpsPython/BuildAndTestZenUnitAndMetalMock.py \
-   --cmake-build-type=Debug \
-   --cmake-definitions="-DFastLinuxDebugBuildMode=ON"
-heaptrack Debug/ZenUnitUtilsAndAssertionTests/ZenUnitUtilsAndAssertionTests --random-seed=0
