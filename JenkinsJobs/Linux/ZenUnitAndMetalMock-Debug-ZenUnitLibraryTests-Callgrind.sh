5b7cf61ffe71dec92a97ea855366c79d06db3328
Neil Justice <njjustice@gmail.com>
2026-07-25 19:44:18 -0500

    Update JenkinsJobs/Linux/ZenUnitAndMetalMock-Debug-ZenUnitLibraryTests-Callgrind.sh to call Callgrind four times

--- JenkinsJobs/Linux/ZenUnitAndMetalMock-Debug-ZenUnitLibraryTests-Callgrind.sh
+++ JenkinsJobs/Linux/ZenUnitAndMetalMock-Debug-ZenUnitLibraryTests-Callgrind.sh
@@ -12,6 +12,25 @@ time valgrind --tool=callgrind \
    --cache-sim=yes \
    --branch-sim=yes \
    Debug/ZenUnitLibraryTests/ZenUnitLibraryTests \
-   --random-seed=0 2> ZenUnitLibraryTests-CallgrindResults.txt
-cat ZenUnitLibraryTests-CallgrindResults.txt
+   --random-seed=0 2> /code/LinuxPerformanceTestResults/CallgrindResults/ZenUnitLibraryTests/ZenUnitLibraryTests-CallgrindResults1.txt
+
+time valgrind --tool=callgrind \
+   --cache-sim=yes \
+   --branch-sim=yes \
+   Debug/ZenUnitLibraryTests/ZenUnitLibraryTests \
+   --random-seed=0 2> /code/LinuxPerformanceTestResults/CallgrindResults/ZenUnitLibraryTests/ZenUnitLibraryTests-CallgrindResults2.txt
+
+time valgrind --tool=callgrind \
+   --cache-sim=yes \
+   --branch-sim=yes \
+   Debug/ZenUnitLibraryTests/ZenUnitLibraryTests \
+   --random-seed=0 2> /code/LinuxPerformanceTestResults/CallgrindResults/ZenUnitLibraryTests/ZenUnitLibraryTests-CallgrindResults3.txt
+
+time valgrind --tool=callgrind \
+   --cache-sim=yes \
+   --branch-sim=yes \
+   Debug/ZenUnitLibraryTests/ZenUnitLibraryTests \
+   --random-seed=0 2> /code/LinuxPerformanceTestResults/CallgrindResults/ZenUnitLibraryTests/ZenUnitLibraryTests-CallgrindResults4.txt
+
+cat /code/LinuxPerformanceTestResults/CallgrindResults/ZenUnitLibraryTests/ZenUnitLibraryTests-CallgrindResults1.txt
 
