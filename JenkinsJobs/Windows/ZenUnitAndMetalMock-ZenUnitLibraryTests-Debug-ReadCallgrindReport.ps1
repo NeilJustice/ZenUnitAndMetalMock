f4e655548429c02ab950f5fef831b1490f111d50
Neil Justice <njjustice@gmail.com>
2026-07-26 21:59:33 -0500

    Updates

--- JenkinsJobs/Windows/ZenUnitAndMetalMock-ZenUnitLibraryTests-Debug-ReadCallgrindReport.ps1
+++ JenkinsJobs/Windows/ZenUnitAndMetalMock-ZenUnitLibraryTests-Debug-ReadCallgrindReport.ps1
@@ -1,3 +1,3 @@
-LinuxCallgrindRunner parse-four-callgrind-results-files-for-instructions `
+LinuxCallgrindRunner parse-four-callgrind-report-files-for-instructions `
    --program-callgrind-reports-folder="C:\Code\LinuxPerformanceTestResults\CallgrindReports\ZenUnitAndMetalMock-ZenUnitLibraryTests" `
    --callgrind-instructions-performance-file="C:\Code\LinuxPerformanceTestResults\CallgrindInstructions\ZenUnitAndMetalMock-ZenUnitLibraryTests-CallgrindInstructions.txt"
