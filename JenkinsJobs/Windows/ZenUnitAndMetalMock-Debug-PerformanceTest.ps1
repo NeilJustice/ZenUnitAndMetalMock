942cf0dbffee89607a354ed564804f0d978106f4
Neil Justice <njjustice@gmail.com>
2026-08-29 18:14:50 -0500

    Update JenkinsJobs/Windows/ZenUnitAndMetalMock-Debug-PerformanceTest.ps1

--- JenkinsJobs/Windows/ZenUnitAndMetalMock-Debug-PerformanceTest.ps1
+++ JenkinsJobs/Windows/ZenUnitAndMetalMock-Debug-PerformanceTest.ps1
@@ -1,6 +1,7 @@
 AcceliTune.exe build-then-measure-program-run-time `
    --working-dir="." `
-   --program="ZenUnitLibraryTests" `
+   --solution=ZenUnitAndMetalMock `
+   --program=ZenUnitLibraryTests `
    --configuration=Debug `
    --args="--test-runs=100" `
    --performance-test-results-file="C:\Code\WindowsPerformanceTestResults\Performance\ZenUnitAndMetalMock-Performance.txt"
