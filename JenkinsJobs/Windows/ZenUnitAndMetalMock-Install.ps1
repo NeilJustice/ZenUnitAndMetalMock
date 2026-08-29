e884f2c7a6cfee7761748081aac07c468e490eed
Neil Justice <njjustice@gmail.com>
2026-08-29 15:09:11 -0500

    Remove JenkinsJobs/Windows/ZenUnitAndMetalMock-Install.ps1

--- JenkinsJobs/Windows/ZenUnitAndMetalMock-Install.ps1
+++
@@ -1,4 +0,0 @@
-cmake.exe . -G"Visual Studio 18 2026" -A"x64" -DCMAKE_INSTALL_PREFIX="C:\\"
-if ($LastExitCode -ne 0) { exit $LastExitCode }
-cmake.exe --build . --target install
-exit $LastExitCode
