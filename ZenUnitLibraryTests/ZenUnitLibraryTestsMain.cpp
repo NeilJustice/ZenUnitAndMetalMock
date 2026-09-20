ecd424896c71dad39beab1ab7a893828ce691869
Neil Justice <njjustice@gmail.com>
2026-09-19 21:05:22 -0500

    Add CALLGRIND_START_INSTRUMENTATION and CALLGRIND_STOP_INSTRUMENTATION to ZenUnitLibraryTests/ZenUnitLibraryTestsMain.cpp

--- ZenUnitLibraryTests/ZenUnitLibraryTestsMain.cpp
+++ ZenUnitLibraryTests/ZenUnitLibraryTestsMain.cpp
@@ -2,7 +2,13 @@
 
 int main(int argc, char* argv[])
 {
+#ifdef __linux__
+   CALLGRIND_START_INSTRUMENTATION;
+#endif
    ZenUnit::globalZenUnitMode.selfTest = true;
    int exitCode = ZenUnit::RunTests(argc, argv);
+#ifdef __linux__
+   CALLGRIND_STOP_INSTRUMENTATION;
+#endif
    return exitCode;
 }
